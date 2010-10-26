edbc53585494dfccb18bbc8ca62b7a052a8a80a7
Che-Liang Chiou <clchiou@chromium.org>
2010-10-26 18:44:47 +0800

    Implement boot device r/w functions for vboot
    BUG=None
    TEST=Manual
    
    Test procedure:
    1. Compile with VBOOT_DEBUG flag on successfully
    2. Run u-boot on dev board as follows:
    (Assume you have mmc device on dev board)
    CrOS> mmc init
    CrOS> cros bootdev set mmc 1
    CrOS> cros bootdev
    (info of mmc1)
    CrOS> read mmc 1 0x20000000 1 2
    CrOS> md.b 0x20000000
    (mmc1 content)
    CrOS> cros bootdev read 0x20001000 1 2
    CrOS> md.b 0x20001000
    (mmc1 content)
    CrOS> cmp.b 0x20000000 0x20001000 0x400
    (compare the blocks, should be identical)
    
    Review URL: http://codereview.chromium.org/4001006
    
    Change-Id: I339f9fb63862993f8b9ced80d4c27d6bef0c1bc6

--- config.mk
+++ config.mk
@@ -139,6 +139,9 @@ endif
 ifdef VBOOT
 CPPFLAGS += -I$(VBOOT)/include/vboot
 endif
+ifdef VBOOT_DEBUG
+CPPFLAGS += -DVBOOT_DEBUG
+endif
 
 CPPFLAGS += -I$(TOPDIR)/include
 CPPFLAGS += -fno-builtin -ffreestanding -nostdinc	\
