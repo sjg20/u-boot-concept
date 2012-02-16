debdc05559a847e4041d6a21681d91ca638fa163
Duncan Laurie <dlaurie@chromium.org>
2012-02-16 14:22:49 -0800

    Revert "chromium: Report Ctrl-Enter key to verified boot."
    This reverts commit 33d305f7c5fe0a7b6665ab44116012d5c17f33a8.
    
    BUG=chrome-os-partner:8075
    TEST=none
    
    Change-Id: Ie5a62dd1611866785fdc818b2afcd0144da0ecb8
    Reviewed-on: https://gerrit.chromium.org/gerrit/16077
    Tested-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-by: Ronald G. Minnich <rminnich@chromium.org>

--- lib/vbexport/keyboard.c
+++ lib/vbexport/keyboard.c
@@ -26,16 +26,8 @@ uint32_t VbExKeyboardRead(void)
 		goto out;
 
 	/* Read a non-Escape character or a standalone Escape character. */
-	c = getc();
-	if (c != CSI_0 || !tstc()) {
-		/*
-		 * Special handle of Ctrl-Enter, which is converted into '\n'
-		 * by i8042 driver.
-		 */
-		if (c == '\n')
-			c = VB_KEY_CTRL_ENTER;
+	if ((c = getc()) != CSI_0 || !tstc())
 		goto out;
-	}
 
 	/* Filter out non- Escape-[ sequence. */
 	if (getc() != CSI_1) {
