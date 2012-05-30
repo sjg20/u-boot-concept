1b1b85c4c05baaa302dd3c0694bb504e71ebbffb
Hatim Ali <hatim.rv@samsung.com>
2012-05-30 05:17:54 -0700

    CHERRY-PICK: exynos: fdt: add DP device node
    Add entry for DP device node in fdt.
    
    BUG = chrome-os-partner:9766
    TEST = build ok. Booted with other patches on snow board
    	Can see console on LCD
    
    Change-Id: I3a68248a6e52661b4d84229306bb86133b2f0091
    Signed-off-by: Hatim Ali <hatim.rv@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/24230
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Bernie Thompson <bhthompson@chromium.org>
    Commit-Ready: Bernie Thompson <bhthompson@chromium.org>

--- lib/fdtdec.c
+++ lib/fdtdec.c
@@ -54,6 +54,7 @@ static const char * const compat_names[COMPAT_COUNT] = {
 	COMPAT(GOOGLE_MKBP, "google,mkbp"),
 	COMPAT(GOOGLE_MKBP_KEYB, "google,mkbp-keyb"),
 	COMPAT(GOOGLE_BOARD_REV, "google,board-revision"),
+	COMPAT(SAMSUNG_EXYNOS_DP, "samsung,exynos-dp"),
 };
 
 const char *fdtdec_get_compatible(enum fdt_compat_id id)
