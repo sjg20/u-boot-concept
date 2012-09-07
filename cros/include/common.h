01e373e9c7ddaf6c58f2dc3a837ed680e2bd5d14
Simon Glass <sjg@chromium.org>
2012-09-07 16:24:25 -0700

    cros: Read bmpblk from GBB when the display is inited
    If vboot calls VbExDisplayInit() then we can expect that it will start
    looking through the bitmaps for one to display. Unfortunately on ARM we
    are currently guessing what vboot might need from the GBB, since it takes
    about 200ms to read the whole thing.
    
    Normally we guess correctly, since we can use the VB_INIT_OUT_ENABLE_DISPLAY
    flag to tell us that the display will be needed. This doesn't work for
    software sync, however, so we need to copy with an unexpected call to
    VbExDisplayInit().
    
    This is a work-around. When we come to clean this up we should remove
    CONFIG_HARDWARE_MAPPED_SPI and unify the code paths, as well as change vboot
    to request data from GBB before it uses it.
    
    BUG=chrome-os-partner:13492
    BRANCH=snow
    TEST=manual
    Reflash snow with vboot modified to always software sync. Update U-Boot fdt
    to set preamble-flags to 0. Remove the factory-friendly flags so that
    developer mode is not forced (not strictly necessary).
    
    Do a normal boot and see that the wait screen now appears on snow correctly.
    
    Change-Id: Iae2e34c485383272d4feb1e90c22bf9a4d835ce1
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/32632
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    (cherry picked from commit 66185454a6ad72b5713e66b00221d2c191458efb)
    
    Conflicts:
    
    	cros/include/common.h
    
    Change-Id: I4b93e64a30082ead9ea711d2531cc903446b2209
    Reviewed-on: https://gerrit.chromium.org/gerrit/32860
    Reviewed-by: Katie Roberts-Hoffman <katierh@chromium.org>
    Tested-by: Katie Roberts-Hoffman <katierh@chromium.org>

--- cros/include/common.h
+++ cros/include/common.h
@@ -71,4 +71,11 @@ int display_clear(void);
 int cros_test_swsync(struct mkbp_dev *dev, int region_mask, int force,
 		     int verify);
 
+/**
+ * Ensure that bitmaps are loaded into our gbb area
+ *
+ * @return 0 if ok, -1 on error
+ */
+int cros_cboot_twostop_read_bmp_block(void);
+
 #endif /* CHROMEOS_COMMON_H_ */
