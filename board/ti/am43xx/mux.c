e3dd7138b1f3a3a7a618dc829393f453403dbea4
Lokesh Vutla <lokeshvutla@ti.com>
2013-09-04 18:29:48 +0530

    ARM: AM43xx: mux: Update mux data
    Updating the mux data for AM43xx board.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- board/ti/am43xx/mux.c
+++ board/ti/am43xx/mux.c
@@ -12,8 +12,36 @@
 #include "board.h"
 
 static struct module_pin_mux uart0_pin_mux[] = {
-	{OFFSET(uart0_rxd), (MODE(0) | RXACTIVE)},	/* UART0_RXD */
-	{OFFSET(uart0_txd), (MODE(0))},			/* UART0_TXD */
+	{OFFSET(uart0_rxd),
+	 (MODE(0) | PULLUP_EN | RXACTIVE | SLEWCTRL | DSPULLUDEN)},
+	{OFFSET(uart0_txd),
+	 (MODE(0) | PULLUDDIS | PULLUP_EN | SLEWCTRL | DSPULLUDEN)},
+	{-1},
+};
+
+static struct module_pin_mux mmc0_pin_mux[] = {
+	{OFFSET(mmc0_clk), (MODE(0) | PULLUDDIS | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(mmc0_cmd), (MODE(0) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(mmc0_dat0), (MODE(0) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(mmc0_dat1), (MODE(0) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(mmc0_dat2), (MODE(0) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(mmc0_dat3), (MODE(0) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{-1},
+};
+
+static struct module_pin_mux mmc1_pin_mux[] = {
+	{OFFSET(gpmc_csn1), (MODE(2) | PULLUDDIS | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(gpmc_csn2), (MODE(2) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(gpmc_ad8), (MODE(2) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(gpmc_ad9), (MODE(2) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(gpmc_ad10), (MODE(2) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{OFFSET(gpmc_ad11), (MODE(2) | PULLUP_EN | RXACTIVE | DSPULLUDEN)},
+	{-1},
+};
+
+static struct module_pin_mux i2c0_pin_mux[] = {
+	{OFFSET(i2c0_sda), (MODE(0) | PULLUP_EN | RXACTIVE | SLEWCTRL)},
+	{OFFSET(i2c0_scl), (MODE(0) | PULLUP_EN | RXACTIVE | SLEWCTRL)},
 	{-1},
 };
 
@@ -24,4 +52,7 @@ void enable_uart0_pin_mux(void)
 
 void enable_board_pin_mux(void)
 {
+	configure_module_pin_mux(mmc0_pin_mux);
+	configure_module_pin_mux(mmc1_pin_mux);
+	configure_module_pin_mux(i2c0_pin_mux);
 }
