9b64da44ebb2060d44be8ec7590f0f471ffcd669
Sekhar Nori <nsekhar@ti.com>
2013-10-05 05:47:56 +0530

    ARM: AM43XX: board: add support for reading onboard EEPROM
    Add support for reading onboard EEPROM to enable
    board detection.
    
    While at it, move a variable used only in U-Boot build
    under U-Boot only build to remove an unused variable
    warning in SPL build.
    
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- board/ti/am43xx/board.c
+++ board/ti/am43xx/board.c
@@ -9,6 +9,7 @@
  */
 
 #include <common.h>
+#include <i2c.h>
 #include <spl.h>
 #include <asm/arch/clock.h>
 #include <asm/arch/cpu.h>
@@ -17,16 +18,58 @@
 #include <asm/arch/mux.h>
 #include <asm/arch/ddr_defs.h>
 #include <asm/emif.h>
+#include <asm/errno.h>
 #include "board.h"
 #include <miiphy.h>
 #include <cpsw.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
-static struct ctrl_dev *cdev = (struct ctrl_dev *)CTRL_DEVICE_BASE;
+/*
+ * Read header information from EEPROM into global structure.
+ */
+static int read_eeprom(struct am43xx_board_id *header)
+{
+	/* Check if baseboard eeprom is available */
+	if (i2c_probe(CONFIG_SYS_I2C_EEPROM_ADDR)) {
+		printf("Could not probe the EEPROM at 0x%x; "
+		     "something fundamentally wrong on the I2C bus.\n",
+		     CONFIG_SYS_I2C_EEPROM_ADDR);
+		return -ENODEV;
+	}
+
+	/* read the eeprom using i2c */
+	if (i2c_read(CONFIG_SYS_I2C_EEPROM_ADDR, 0, 2, (uchar *)header,
+		     sizeof(struct am43xx_board_id))) {
+		puts("Could not read the EEPROM; something fundamentally"
+			" wrong on the I2C bus.\n");
+		return -EIO;
+	}
+
+	if (header->magic != 0xEE3355AA) {
+		/*
+		 * read the eeprom using i2c again,
+		 * but use only a 1 byte address
+		 */
+		if (i2c_read(CONFIG_SYS_I2C_EEPROM_ADDR, 0, 1, (uchar *)header,
+			     sizeof(struct am43xx_board_id))) {
+			printf("Could not read the EEPROM at 0x%x; something "
+			     "fundamentally wrong on the I2C bus.\n",
+			     CONFIG_SYS_I2C_EEPROM_ADDR);
+			return -EIO;
+		}
+
+		if (header->magic != 0xEE3355AA) {
+			printf("Incorrect magic number (0x%x) in EEPROM\n",
+					header->magic);
+			return -EINVAL;
+		}
+	}
+
+	return 0;
+}
 
 #ifdef CONFIG_SPL_BUILD
-
 const struct dpll_params dpll_ddr = {
 		266, 24, 1, -1, 1, -1, -1};
 
@@ -122,6 +165,8 @@ static struct cpsw_platform_data cpsw_data = {
 	.host_port_num		= 0,
 	.version		= CPSW_CTRL_VERSION_2,
 };
+
+static struct ctrl_dev *cdev = (struct ctrl_dev *)CTRL_DEVICE_BASE;
 
 int board_eth_init(bd_t *bis)
 {
