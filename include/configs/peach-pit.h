f3ce230014c06dd8001d3e79f68962c7c143a3f1
Simon Glass <sjg@chromium.org>
2014-07-20 05:47:37 +0100

    config: Move arndale to use common exynos5250 file
    Most of the arndale features are common with other exynos5250 boards. To
    permit easier addition of driver model support, use the common file.
    
    Series-changes: 2
    - Reduce the number of common elements to avoid needing #undefs later
    
    Change-Id: I72d05e00a3ac88650ce3e8e5a62b5e3d99abb27a
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/configs/peach-pit.h
+++ include/configs/peach-pit.h
@@ -9,8 +9,16 @@
 #ifndef __CONFIG_PEACH_PIT_H
 #define __CONFIG_PEACH_PIT_H
 
+#define CONFIG_ENV_IS_IN_SPI_FLASH
+#define CONFIG_SPI_FLASH
+#define CONFIG_ENV_SPI_BASE	0x12D30000
+#define FLASH_SIZE		(0x4 << 20)
+#define CONFIG_ENV_OFFSET	(FLASH_SIZE - CONFIG_BL2_SIZE)
+
 #include <configs/exynos5420-common.h>
 #include <configs/exynos5-dt-common.h>
+
+#define CONFIG_BOARD_COMMON
 
 #undef CONFIG_DEFAULT_DEVICE_TREE
 #define CONFIG_DEFAULT_DEVICE_TREE	exynos5420-peach-pit
@@ -33,5 +41,8 @@
 
 #define CONFIG_POWER_TPS65090_EC
 #define CONFIG_CROS_EC_SPI		/* Support CROS_EC over SPI */
+
+#define CONFIG_USB_XHCI
+#define CONFIG_USB_XHCI_EXYNOS
 
 #endif	/* __CONFIG_PEACH_PIT_H */
