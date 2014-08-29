archlinuxarm/u-boot-chromebook2

gen: Provide option to avoid defining a custom version of uintptr_t.
There's a definition in stdint.h (provided by gcc) which will be more correct
if available.

Define CONFIG_USE_STDINT to use this feature, or USE_STDINT=1 on the 'make'
commmand.

BUG=None
TEST=Built for Link, Daisy, and Lumpy.
BRANCH=None

Change-Id: I207075123c8349a3dc7bc5e6a82b0d668c19616f
Signed-off-by: Gabe Black <gabeblack@google.com>
Reviewed-on: https://gerrit.chromium.org/gerrit/45689
Commit-Queue: Gabe Black <gabeblack@chromium.org>
Reviewed-by: Gabe Black <gabeblack@chromium.org>
Tested-by: Gabe Black <gabeblack@chromium.org>
Reviewed-on: https://gerrit-int.chromium.org/34014
Reviewed-by: Bill Richardson <wfrichar@google.com>

Rewritten to be an option, since stdint.h is often available only in glibc.
Signed-off-by: Simon Glass <sjg@chromium.org>

config.mk
@@ -238,6 +238,11 @@ Please undefined CONFIG_SYS_GENERIC_BOARD in your board config file)
 endif
 endif
 
+# Allow use of stdint.h if available
+ifneq ($(USE_STDINT),)
+CPPFLAGS += -DCONFIG_USE_STDINT
+endif
+
 ifneq ($(RESET_VECTOR_ADDRESS),)
 CPPFLAGS += -DRESET_VECTOR_ADDRESS=$(RESET_VECTOR_ADDRESS)
 endif

sjg20/u-boot-concept

Conflicts:
	README
	config.mk

config.mk
@@ -58,6 +58,11 @@ ifeq ($(WERROR),y)
 PLATFORM_CPPFLAGS += -Werror
 endif
 
+# Allow use of stdint.h if available
+ifneq ($(USE_STDINT),)
+PLATFORM_CPPFLAGS += -DCONFIG_USE_STDINT
+endif
+
 ifneq ($(CONFIG_CHROMEOS),)
 PLATFORM_CPPFLAGS += -I$(srctree)/cros/include
 endif