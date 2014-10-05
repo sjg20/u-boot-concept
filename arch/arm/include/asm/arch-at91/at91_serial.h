8cd05351ff8fd6d520903b50c489136f89432aa7
Simon Glass <sjg@chromium.org>
2014-10-04 20:44:19 -0600

    dm: at91: Add driver model support for the serial driver
    Add driver model support while retaining the existing legacy code. This
    allows the driver to support boards that have converted to driver model
    as well as those that have not.
    
    Change-Id: I781a862f74a521b4b80177cb7db63abf6b6d1cb8
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ arch/arm/include/asm/arch-at91/at91_serial.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2014 Google, Inc
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef _AT91_SERIAL_H
+#define _AT91_SERIAL_H
+
+/* Information about a serial port */
+struct at91_serial_platdata {
+	uint32_t base_addr;
+};
+
+#endif
