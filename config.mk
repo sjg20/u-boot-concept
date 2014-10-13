archlinuxarm/u-boot-chromebook2

cros: Add build type controls CROS_RO, CROS_RW, CROS_SMALL, CROS_LARGE
These controlls will permit us to create customized versions of U-Boot if
we find this to be beneficial:

CROS_LARGE
	- Full version as now, all features enabled

CROS_SMALL
	- Minimized for use only with verified boot

CROS_RO
	- Requires CROS_SMALL. Will only support running RO firmware

CROS_RW
	- Requires CROS_SMALL. Will only support running RW firmware

BUG=chromium:247392
TEST=manual
Build (no change in binary)

Change-Id: I22e032c618175155f416e958efbe90a8debf4417
Signed-off-by: Simon Glass <sjg@chromium.org>
Reviewed-on: https://gerrit.chromium.org/gerrit/61063
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>

config.mk
@@ -275,6 +275,22 @@ ifneq ($(CONFIG_CHROMEOS),)
 CPPFLAGS += -I$(TOPDIR)/cros/include
 endif
 
+ifdef CROS_RO
+CPPFLAGS += -DCONFIG_CROS_RO
+endif
+
+ifdef CROS_RW
+CPPFLAGS += -DCONFIG_CROS_RW
+endif
+
+ifdef CROS_SMALL
+CPPFLAGS += -DCONFIG_CROS_SMALL
+endif
+
+ifdef CROS_FULL
+CPPFLAGS += -DCONFIG_CROS_FULL
+endif
+
 ifdef CONFIG_CHROMEOS
 CPPFLAGS += -I$(if $(VBOOT_SOURCE),$(VBOOT_SOURCE)/firmware/include,\
 		$(VBOOT)/include/vboot) \

sjg20/u-boot-concept

config.mk
@@ -71,6 +71,22 @@ ifneq ($(CONFIG_CHROMEOS),)
 PLATFORM_CPPFLAGS += -I$(srctree)/cros/include
 endif
 
+ifdef CROS_RO
+PLATFORM_CPPFLAGS += -DCONFIG_CROS_RO
+endif
+
+ifdef CROS_RW
+PLATFORM_CPPFLAGS += -DCONFIG_CROS_RW
+endif
+
+ifdef CROS_SMALL
+PLATFORM_CPPFLAGS += -DCONFIG_CROS_SMALL
+endif
+
+ifdef CROS_FULL
+PLATFORM_CPPFLAGS += -DCONFIG_CROS_FULL
+endif
+
 ifdef CONFIG_CHROMEOS
 PLATFORM_CPPFLAGS += -I$(if $(VBOOT_SOURCE),$(VBOOT_SOURCE)/firmware/include,\
 		$(VBOOT)/include/vboot) \