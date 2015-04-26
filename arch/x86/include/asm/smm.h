690e68ac7a41203f9c7f8e57adcbad1b29d6c3e3
Simon Glass <sjg@chromium.org>
2015-04-26 16:07:07 -0600

    x86: Add multi-processor init
    Most modern x86 CPUs include more than one CPU core. The OS normally requires
    that these 'Application Processors' (APs) be brought up by the boot loader.
    Add the required support to U-Boot to init additional APs.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ arch/x86/include/asm/smm.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2015 Google, Inc
+ * Copyright (C) 2008-2009 coresystems GmbH
+ *
+ * SPDX-License-Identifier:	GPL-2.0
+ */
+
+#ifndef CPU_X86_SMM_H
+#define CPU_X86_SMM_H
+
+#define SMM_DEFAULT_BASE 0x30000
+#define SMM_DEFAULT_SIZE 0x10000
+
+#endif
