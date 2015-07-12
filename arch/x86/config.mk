70839c0e3868bd84ad0a6dc42bfbbf800f144d08
Ben Stoltz <stoltz@google.com>
2015-07-12 15:05:45 -0600

    x86: Set up tool chain flags for running as EFI application
    Adjust the tool chain flags to build U-Boot as a relocatable shared library,
    as required by EFI.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- arch/x86/config.mk
+++ arch/x86/config.mk
@@ -8,7 +8,6 @@
 CONFIG_STANDALONE_LOAD_ADDR ?= 0x40000
 
 PLATFORM_CPPFLAGS += -fno-strict-aliasing
-PLATFORM_CPPFLAGS += -mregparm=3
 PLATFORM_CPPFLAGS += -fomit-frame-pointer
 PF_CPPFLAGS_X86   := $(call cc-option, -fno-toplevel-reorder, \
 		       $(call cc-option, -fno-unit-at-a-time)) \
@@ -19,8 +18,33 @@ PLATFORM_CPPFLAGS += -march=i386 -m32
 
 PLATFORM_RELFLAGS += -ffunction-sections -fvisibility=hidden
 
-PLATFORM_LDFLAGS += --emit-relocs -Bsymbolic -Bsymbolic-functions -m elf_i386
+PLATFORM_LDFLAGS += -Bsymbolic -Bsymbolic-functions -m elf_i386
 
-LDFLAGS_FINAL += --gc-sections -pie
 LDFLAGS_FINAL += --wrap=__divdi3 --wrap=__udivdi3
 LDFLAGS_FINAL += --wrap=__moddi3 --wrap=__umoddi3
+
+OBJCOPYFLAGS_EFI := -j .text -j .sdata -j .data -j .dynamic -j .dynsym \
+	-j .rel -j .rela -j .reloc
+
+ifeq ($(CONFIG_ARCH_EFI),y)
+
+ifeq ($(ARCH),x86)
+EFILIB=/usr/lib32
+EFIARCH=ia32
+else
+EFILIB=/usr/lib
+EFIARCH=x86_64
+endif
+
+PLATFORM_CPPFLAGS += -fpic -fshort-wchar
+LDFLAGS_FINAL += -znocombreloc -shared
+LDSCRIPT := $(srctree)/$(CPUDIR)/efi/elf_ia32_efi.lds
+OBJCOPYFLAGS_EFI += --target=efi-app-ia32
+
+else
+
+PLATFORM_CPPFLAGS += -mregparm=3
+PLATFORM_LDFLAGS += --emit-relocs
+LDFLAGS_FINAL += --gc-sections -pie
+
+endif
