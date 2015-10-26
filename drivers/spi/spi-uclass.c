14991c9991d9da8ff04d1c69f1051d3fc6c1fc8d
Mugunthan V N <mugunthanvnm@ti.com>
2015-10-26 23:18:45 +0530

    drivers: spi: spi-uclass: fix spi slave device register
    when a device registered with device_bind_driver(), its fdt node
    is not added to the device. Due to this, in spi_child_post_bind()
    dev->of_offset is -1 and spi device fdt is never parsed. So
    changing the device register by device_bind_driver_to_node() so
    that the spi slave device is registered with of_offset and its
    fdt is parsed properly.
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- drivers/spi/spi-uclass.c
+++ drivers/spi/spi-uclass.c
@@ -259,10 +259,27 @@ int spi_get_bus_and_cs(int busnum, int cs, int speed, int mode,
 	 */
 	if (ret == -ENODEV && drv_name) {
 		struct dm_spi_slave_platdata *plat;
+		const void *fdt = gd->fdt_blob;
+		int node = bus->of_offset;
+		int subnode = -1;
+		int slave_cs = -1;
 
 		debug("%s: Binding new device '%s', busnum=%d, cs=%d, driver=%s\n",
 		      __func__, dev_name, busnum, cs, drv_name);
-		ret = device_bind_driver(bus, drv_name, dev_name, &dev);
+
+		fdt_for_each_subnode(fdt, subnode, node) {
+			slave_cs = fdtdec_get_int(fdt, subnode, "reg", -1);
+			if(cs == slave_cs)
+				break;
+		}
+
+		if(cs != slave_cs) {
+			printf("%s: device not found in device tree blob\n",
+			       __func__);
+			subnode = -1;
+		}
+
+		ret = device_bind_driver_to_node(bus, drv_name, dev_name, subnode, &dev);
 		if (ret)
 			return ret;
 		plat = dev_get_parent_platdata(dev);
