9a8c8ab1272cf5f00895c5ab312f27f8378b1240
Mugunthan V N <mugunthanvnm@ti.com>
2015-11-04 13:46:19 +0530

    drivers: mtd: spi: sf_probe: add compatible for Macronix spi flash
    Add compatible for Macronix 64MiB spi flash mx66l51235l.
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- drivers/mtd/spi/sf_probe.c
+++ drivers/mtd/spi/sf_probe.c
@@ -516,6 +516,7 @@ static const struct dm_spi_flash_ops spi_flash_std_ops = {
 static const struct udevice_id spi_flash_std_ids[] = {
 	{ .compatible = "spi-flash" },
 	{ .compatible = "s25fl256s1" },
+	{ .compatible = "mx66l51235l" },
 	{ }
 };
 
