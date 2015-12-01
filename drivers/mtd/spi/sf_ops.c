86ae2887859aded181fcea041da7176c072bcb98
Mugunthan V N <mugunthanvnm@ti.com>
2015-12-01 14:31:12 +0530

    sf: sf_ops: use dma to copy data from mmap region if platform supports
    Add dma memcpy api to the default spi_flash_copy_mmap(), so that
    dma will be used to copy data when DM_DMA is defined for the
    platform.
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- drivers/mtd/spi/sf_ops.c
+++ drivers/mtd/spi/sf_ops.c
@@ -16,6 +16,7 @@
 #include <watchdog.h>
 #include <linux/compiler.h>
 #include <linux/log2.h>
+#include <dma.h>
 
 #include "sf_internal.h"
 
@@ -389,6 +390,10 @@ int spi_flash_read_common(struct spi_flash *flash, const u8 *cmd,
 
 void __weak spi_flash_copy_mmap(void *data, void *offset, size_t len)
 {
+#ifdef CONFIG_DM_DMA
+	if (!dma_memcpy(data, offset, len))
+		return;
+#endif
 	memcpy(data, offset, len);
 }
 
