96869f7bd8d0b2a8d864176500663b3159dfeb4d
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-12-18 20:15:14 +0900

    clk: add enable() callback
    The most basic thing for clock is to enable it, but it is missing
    in this uclass.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- include/clk.h
+++ include/clk.h
@@ -33,6 +33,15 @@ struct clk_ops {
 	ulong (*set_rate)(struct udevice *dev, ulong rate);
 
 	/**
+	 * enable() - Enable the clock for a peripheral
+	 *
+	 * @dev:	clock provider
+	 * @periph:	Peripheral ID to enable
+	 * @return zero on success, or -ve error code
+	 */
+	int (*enable)(struct udevice *dev, int periph);
+
+	/**
 	 * get_periph_rate() - Get clock rate for a peripheral
 	 *
 	 * @dev:	Device to check (UCLASS_CLK)
