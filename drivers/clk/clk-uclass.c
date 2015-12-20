468570567651d653a911fb7f8eb73514a0e7b5d8
Simon Glass <sjg@chromium.org>
2015-12-20 11:26:09 -0700

    dm: clk: Add support for decoding clocks from the device tree
    Add a method which can locate a clock for a device, given its index. This
    uses the normal device tree bindings to return the clock device and the
    first argument which is normally used as a peripheral ID in U-Boot.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/clk/clk-uclass.c
+++ drivers/clk/clk-uclass.c
@@ -12,6 +12,8 @@
 #include <dm/lists.h>
 #include <dm/root.h>
 
+DECLARE_GLOBAL_DATA_PTR;
+
 ulong clk_get_rate(struct udevice *dev)
 {
 	struct clk_ops *ops = clk_get_ops(dev);
@@ -60,6 +62,32 @@ int clk_get_id(struct udevice *dev, int args_count, uint32_t *args)
 		return -ENOSYS;
 
 	return ops->get_id(dev, args_count, args);
+}
+
+int clk_get_by_index(struct udevice *dev, int index, struct udevice **clk_devp,
+		     int *periphp)
+{
+	struct fdtdec_phandle_args args;
+	int ret;
+
+	ret = fdtdec_parse_phandle_with_args(gd->fdt_blob, dev->of_offset,
+					     "clocks", "#clock-cells", 0, index,
+					     &args);
+	if (ret) {
+		debug("%s: fdtdec_parse_phandle_with_args failed: err=%d\n",
+		      __func__, ret);
+		return ret;
+	}
+
+	ret = uclass_get_device_by_of_offset(UCLASS_CLK, args.node, clk_devp);
+	if (ret) {
+		debug("%s: uclass_get_device_by_of_offset failed: err=%d\n",
+		      __func__, ret);
+		return ret;
+	}
+	*periphp = args.args_count > 0 ? args.args[0] : -1;
+
+	return 0;
 }
 
 UCLASS_DRIVER(clk) = {
