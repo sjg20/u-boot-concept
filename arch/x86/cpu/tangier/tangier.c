34fcbedf3f95ad4d250dfde9885ab2d9f1efe436
Bin Meng <bmeng.cn@gmail.com>
2018-07-03 17:03:23 +0800

    x86: Switch to use DM sysreset driver
    This converts all x86 boards over to DM sysreset.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    
    Series-to: Simon, Stefan, George, Andy, U-Boot

--- arch/x86/cpu/tangier/tangier.c
+++ arch/x86/cpu/tangier/tangier.c
@@ -25,7 +25,15 @@ int print_cpuinfo(void)
 	return default_print_cpuinfo();
 }
 
+/* TODO: convert to DM sysreset */
 void reset_cpu(ulong addr)
 {
 	scu_ipc_simple_command(IPCMSG_COLD_RESET, 0);
+}
+
+int do_reset(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	reset_cpu(0);
+
+	return 0;
 }
