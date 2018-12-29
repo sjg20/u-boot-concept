3b16a55b7eb83d698df597947193bd67d00c04e9
Simon Glass <sjg@chromium.org>
2018-12-29 12:51:54 -0700

    wip

--- arch/arm/config.mk
+++ arch/arm/config.mk
@@ -64,8 +64,8 @@ endif
 
 checkgcc6:
 	@if test "$(call cc-name)" = "gcc" -a \
-			"$(call cc-version)" -lt "0600"; then \
-		echo '*** Your GCC is older than 6.0 and is not supported'; \
+			"$(call cc-version)" -lt "0400"; then \
+		echo '*** Your GCC is older than 4.0 and is not supported'; \
 		false; \
 	fi
 
--- common/spl/cb.c
+++ common/spl/cb.c
@@ -1043,7 +1043,6 @@ void cb_main(void)
 // 	if (IS_ENABLED(CONFIG_COLLECT_TIMESTAMPS))
 // 		base_timestamp = timestamp_get();
 
-	fred("U-Boot SPL");
 	bootblock_main_with_timestamp(base_timestamp, NULL, 0);
 }
 
@@ -1071,4 +1070,28 @@ void *memset(void *ptr,int ch, __kernel_size_t count)
 		*s8++ = ch;
 
 	return ptr;
+}
+
+void * memcpy(void *dest, const void *src, size_t count)
+{
+	unsigned long *dl = (unsigned long *)dest, *sl = (unsigned long *)src;
+	char *d8, *s8;
+
+	if (src == dest)
+		return dest;
+
+	/* while all data is aligned (common case), copy a word at a time */
+	if ( (((ulong)dest | (ulong)src) & (sizeof(*dl) - 1)) == 0) {
+		while (count >= sizeof(*dl)) {
+			*dl++ = *sl++;
+			count -= sizeof(*dl);
+		}
+	}
+	/* copy the reset one byte at a time */
+	d8 = (char *)dl;
+	s8 = (char *)sl;
+	while (count--)
+		*d8++ = *s8++;
+
+	return dest;
 }
