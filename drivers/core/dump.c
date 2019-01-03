u-boot/u-boot

commit 5197dafc42eb926525a12fc5aa38d2a05f22d177

dm: core: Widen the dump tree to show more of the driver's name.
With drivers that have prefix names that are quite long (like
'versatile_') it is useful to have a wider column for the driver's
name when dumping the device driver tree.

Also update the tests to take into account the wider output format.

Signed-off-by: Liviu Dudau <liviu.dudau@foss.arm.com>
Reviewed-by: Simon Glass <sjg@chromium.org>

drivers/core/dump.c
@@ -15,8 +15,8 @@ static void show_devices(struct udevice *dev, int depth, int last_flag)
 	int i, is_last;
 	struct udevice *child;
 
-	/* print the first 11 characters to not break the tree-format. */
-	printf(" %-10.10s  %d  [ %c ]   %-10.10s  ", dev->uclass->uc_drv->name,
+	/* print the first 20 characters to not break the tree-format. */
+	printf(" %-10.10s  %d  [ %c ]   %-20.20s  ", dev->uclass->uc_drv->name,
 	       dev_get_uclass_index(dev, NULL),
 	       dev->flags & DM_FLAG_ACTIVATED ? '+' : ' ', dev->driver->name);
 
@@ -49,8 +49,8 @@ void dm_dump_all(void)
 
 	root = dm_root();
 	if (root) {
-		printf(" Class    index  Probed  Driver      Name\n");
-		printf("-----------------------------------------\n");
+		printf(" Class    index  Probed  Driver                Name\n");
+		printf("-----------------------------------------------------------\n");
 		show_devices(root, -1, 0);
 	}
 }

test/py/tests/test_bind.py
@@ -13,7 +13,7 @@ def in_tree(response, name, uclass, drv, depth, last_child):
 	else:
 		leaf = leaf + '`'
 	leaf = leaf + '-- ' + name
-	line = ' *{:10.10}  [0-9]*  \[ [ +] \]   {:10.10}  {}$'.format(uclass, drv,leaf)
+	line = ' *{:10.10}  [0-9]*  \[ [ +] \]   {:20.20}  {}$'.format(uclass, drv, leaf)
 	prog = re.compile(line)
 	for l in lines:
 		if prog.match(l):
@@ -28,31 +28,31 @@ def test_bind_unbind_with_node(u_boot_console):
 	response = u_boot_console.run_command("bind  /bind-test generic_simple_bus")
 	assert response == ''
 	tree = u_boot_console.run_command("dm tree")
-	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple", 0, True)
+	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple_bus", 0, True)
 	assert in_tree(tree, "bind-test-child1", "phy", "phy_sandbox", 1, False)
-	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple", 1, True)
+	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple_bus", 1, True)
 
 	#Unbind child #1. No error expected and all devices should be there except for bind-test-child1
 	response = u_boot_console.run_command("unbind  /bind-test/bind-test-child1")
 	assert response == ''
 	tree = u_boot_console.run_command("dm tree")
-	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple", 0, True)
+	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple_bus", 0, True)
 	assert "bind-test-child1" not in tree
-	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple", 1, True)
+	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple_bus", 1, True)
 
 	#bind child #1. No error expected and all devices should be there
 	response = u_boot_console.run_command("bind  /bind-test/bind-test-child1 phy_sandbox")
 	assert response == ''
 	tree = u_boot_console.run_command("dm tree")
-	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple", 0, True)
+	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple_bus", 0, True)
 	assert in_tree(tree, "bind-test-child1", "phy", "phy_sandbox", 1, True)
-	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple", 1, False)
+	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple_bus", 1, False)
 
 	#Unbind child #2. No error expected and all devices should be there except for bind-test-child2
 	response = u_boot_console.run_command("unbind  /bind-test/bind-test-child2")
 	assert response == ''
 	tree = u_boot_console.run_command("dm tree")
-	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple", 0, True)
+	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple_bus", 0, True)
 	assert in_tree(tree, "bind-test-child1", "phy", "phy_sandbox", 1, True)
 	assert "bind-test-child2" not in tree
 
@@ -61,9 +61,9 @@ def test_bind_unbind_with_node(u_boot_console):
 	response = u_boot_console.run_command("bind /bind-test/bind-test-child2 generic_simple_bus")
 	assert response == ''
 	tree = u_boot_console.run_command("dm tree")
-	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple", 0, True)
+	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple_bus", 0, True)
 	assert in_tree(tree, "bind-test-child1", "phy", "phy_sandbox", 1, False)
-	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple", 1, True)
+	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple_bus", 1, True)
 
 	#Unbind parent. No error expected. All devices should be removed and unbound
 	response = u_boot_console.run_command("unbind  /bind-test")
@@ -89,9 +89,9 @@ def test_bind_unbind_with_node(u_boot_console):
 	response = u_boot_console.run_command("bind  /bind-test generic_simple_bus")
 	assert response == ''
 	tree = u_boot_console.run_command("dm tree")
-	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple", 0, True)
+	assert in_tree(tree, "bind-test", "simple_bus", "generic_simple_bus", 0, True)
 	assert in_tree(tree, "bind-test-child1", "phy", "phy_sandbox", 1, False)
-	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple", 1, True)
+	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple_bus", 1, True)
 
 	response = u_boot_console.run_command("unbind  /bind-test")
 	assert response == ''
@@ -138,7 +138,7 @@ def test_bind_unbind_with_uclass(u_boot_console):
 	response = u_boot_console.run_command("unbind  simple_bus {}".format(child_of_child2_index))
 	assert response == ''
 	tree = u_boot_console.run_command("dm tree")
-	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple", 1, True)
+	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple_bus", 1, True)
 	assert not in_tree(tree, "generic_simple_bus", "simple_bus", "generic_simple_bus", 2, True)
 	child_of_child2_line = get_next_line(tree, "bind-test-child2")
 	assert child_of_child2_line == ""
@@ -161,7 +161,7 @@ def test_bind_unbind_with_uclass(u_boot_console):
 	assert response == ''
 
 	tree = u_boot_console.run_command("dm tree")
-	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple", 1, True)
+	assert in_tree(tree, "bind-test-child2", "simple_bus", "generic_simple_bus", 1, True)
 
 	child_of_child2_line = get_next_line(tree, "bind-test-child2")
 	assert child_of_child2_line == ""

sjg20/u-boot-concept

commit 46a36b18a0ed4f3da3bbff287d680248b68d82d0

dm: Tidy up 'dm tree' output when there are many devices
At present the 'Index' column assumes there is only one digit. But on some
devices (e.g. snow) there are a lot of regulators and GPIO banks. Adjust
the output to allow for two digits without messing up the display.

Also capatalise the heading to match.

Fixes: 5197dafc42 (dm: core: Widen the dump tree to show more of the
driver's name.)

Signed-off-by: Simon Glass <sjg@chromium.org>
Reviewed-by: Liviu Dudau <liviu.dudau@foss.arm.com>

drivers/core/dump.c
@@ -16,7 +16,7 @@ static void show_devices(struct udevice *dev, int depth, int last_flag)
 	struct udevice *child;
 
 	/* print the first 20 characters to not break the tree-format. */
-	printf(" %-10.10s  %d  [ %c ]   %-20.20s  ", dev->uclass->uc_drv->name,
+	printf(" %-10.10s  %2d  [ %c ]   %-20.20s  ", dev->uclass->uc_drv->name,
 	       dev_get_uclass_index(dev, NULL),
 	       dev->flags & DM_FLAG_ACTIVATED ? '+' : ' ', dev->driver->name);
 
@@ -49,7 +49,7 @@ void dm_dump_all(void)
 
 	root = dm_root();
 	if (root) {
-		printf(" Class    index  Probed  Driver                Name\n");
+		printf(" Class     Index  Probed  Driver                Name\n");
 		printf("-----------------------------------------------------------\n");
 		show_devices(root, -1, 0);
 	}

test/py/tests/test_bind.py
@@ -13,7 +13,8 @@ def in_tree(response, name, uclass, drv, depth, last_child):
 	else:
 		leaf = leaf + '`'
 	leaf = leaf + '-- ' + name
-	line = ' *{:10.10}  [0-9]*  \[ [ +] \]   {:20.20}  {}$'.format(uclass, drv, leaf)
+	line = (' *{:10.10}   [0-9]*  \[ [ +] \]   {:20.20}  {}$'
+	        .format(uclass, drv, leaf))
 	prog = re.compile(line)
 	for l in lines:
 		if prog.match(l):