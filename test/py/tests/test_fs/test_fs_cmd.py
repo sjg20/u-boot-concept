bb81b7c0b138d242f061caa7829728f68a40fa8d
Niel Fourie <lusus@denx.de>
2020-03-24 16:17:04 +0100

    cmd: fs: Add command to list supported fs types
    Added command "fstypes" to list supported/included filesystems.
    
    Signed-off-by: Niel Fourie <lusus@denx.de>
    Cc: Simon Glass <sjg@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

---
+++ test/py/tests/test_fs/test_fs_cmd.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020
+# Niel Fourie, DENX Software Engineering, lusus@denx.de
+
+import pytest
+
+@pytest.mark.buildconfigspec('cmd_fs_generic')
+def test_dm_compat(u_boot_console):
+    """Test that `fstypes` prints a result which includes `sandbox`."""
+    output = u_boot_console.run_command('fstypes')
+    assert "Supported filesystems:" in output
+    assert "sandbox" in output
