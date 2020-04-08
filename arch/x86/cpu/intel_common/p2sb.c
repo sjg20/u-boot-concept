sglass68/u-boot

x86: apl: Hide the p2sb on exit from U-Boot
This confuses Linux's PCI probing so needs to be hidden when booting
Linux. Add a remove() method to handle this.

Change-Id: I68d05c644cec1ef05b0845010253943c44385616
Signed-off-by: Simon Glass <sjg@chromium.org>

arch/x86/cpu/apollolake/p2sb.c
@@ -173,6 +173,17 @@ static int apl_p2sb_set_hide(struct udevice *dev, bool hide)
 	return 0;
 }
 
+static int apl_p2sb_remove(struct udevice *dev)
+{
+	int ret;
+
+	ret = apl_p2sb_set_hide(dev, true);
+	if (ret)
+		return log_msg_ret("hide", ret);
+
+	return 0;
+}
+
 static int p2sb_child_post_bind(struct udevice *dev)
 {
 #if !CONFIG_IS_ENABLED(OF_PLATDATA)
@@ -203,9 +214,12 @@ U_BOOT_DRIVER(apl_p2sb_drv) = {
 	.id		= UCLASS_P2SB,
 	.of_match	= apl_p2sb_ids,
 	.probe		= apl_p2sb_probe,
+	.remove		= apl_p2sb_remove,
+	.ops		= &apl_p2sb_ops,
 	.ofdata_to_platdata = apl_p2sb_ofdata_to_platdata,
 	.platdata_auto_alloc_size = sizeof(struct p2sb_platdata),
 	.per_child_platdata_auto_alloc_size =
 		sizeof(struct p2sb_child_platdata),
 	.child_post_bind = p2sb_child_post_bind,
+	.flags		= DM_FLAG_OS_PREPARE,
 };

sjg20/u-boot-concept

arch/x86/cpu/intel_common/p2sb.c
@@ -151,6 +151,17 @@ static int intel_p2sb_set_hide(struct udevice *dev, bool hide)
 	return 0;
 }
 
+static int p2sb_remove(struct udevice *dev)
+{
+	int ret;
+
+	ret = intel_p2sb_set_hide(dev, true);
+	if (ret)
+		return log_msg_ret("hide", ret);
+
+	return 0;
+}
+
 static int p2sb_child_post_bind(struct udevice *dev)
 {
 #if !CONFIG_IS_ENABLED(OF_PLATDATA)
@@ -181,9 +192,12 @@ U_BOOT_DRIVER(p2sb_drv) = {
 	.id		= UCLASS_P2SB,
 	.of_match	= p2sb_ids,
 	.probe		= p2sb_probe,
+	.remove		= p2sb_remove,
+	.ops		= &p2sb_ops,
 	.ofdata_to_platdata = p2sb_ofdata_to_platdata,
 	.platdata_auto_alloc_size = sizeof(struct p2sb_platdata),
 	.per_child_platdata_auto_alloc_size =
 		sizeof(struct p2sb_child_platdata),
 	.child_post_bind = p2sb_child_post_bind,
+	.flags		= DM_FLAG_OS_PREPARE,
 };