1a23bddf8ef4e0a0cd12310f61fb270a46300d4a
Simon Glass <sjg@chromium.org>
2020-04-26 05:34:40 -0600

    dm: acpi: Enhance acpi_get_name()
    For many device types it is possible to figure out the name just by
    looking at its uclass or parent. Add a function to handle this, since it
    allows us to cover the vast majority of cases automatically.
    
    However it is sometimes impossible to figure out an ACPI name for a device
    just by looking at its uclass. For example a touch device may have a
    vendor-specific name. Add a new "acpi,name" property to allow a custom
    name to be created.
    
    With this new feature we can drop the get_name() methods in the sandbox
    I2C and SPI drivers. They were only added for testing purposes. Update the
    tests to use the new values.
    
    Change-Id: I1a77e678d7c6973796cc651ac6d858e1122bb7ae
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Series-changes: 3
    - Use acpi,ddn instead of acpi,desc
    
    Series-changes: 8
    - Rename to acpi_device_infer_name()
    - Update newly created sandbox tests

--- drivers/i2c/sandbox_i2c.c
+++ drivers/i2c/sandbox_i2c.c
@@ -83,15 +83,6 @@ static int sandbox_i2c_xfer(struct udevice *bus, struct i2c_msg *msg,
 	return ops->xfer(emul, msg, nmsgs);
 }
 
-static int sandbox_i2c_get_name(const struct udevice *dev, char *out_name)
-{
-	return acpi_copy_name(out_name, "SI2C");
-}
-
-struct acpi_ops sandbox_i2c_acpi_ops = {
-	.get_name	= sandbox_i2c_get_name,
-};
-
 static const struct dm_i2c_ops sandbox_i2c_ops = {
 	.xfer		= sandbox_i2c_xfer,
 };
@@ -107,5 +98,4 @@ U_BOOT_DRIVER(i2c_sandbox) = {
 	.of_match = sandbox_i2c_ids,
 	.ops	= &sandbox_i2c_ops,
 	.priv_auto_alloc_size = sizeof(struct sandbox_i2c_priv),
-	ACPI_OPS_PTR(&sandbox_i2c_acpi_ops)
 };
