d56f631afc70ca78feb1a914eaa96174e59f86c6
Simon Glass <sjg@chromium.org>
2020-05-03 11:06:05 -0600

    bdinfo: ppc: Drop arch-specific print_baudrate()
    This function outputs the same basic info. Since the baud rate is commonly
    115200 these is often no difference. Drop the arch-specific code and
    inline it to avoid a one-line function.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- cmd/bdinfo.c
+++ cmd/bdinfo.c
@@ -137,15 +137,6 @@ static void print_eth_ip_addr(void)
 #endif
 }
 
-static void print_baudrate(void)
-{
-#if defined(CONFIG_PPC)
-	printf("baudrate    = %6u bps\n", gd->baudrate);
-#else
-	printf("baudrate    = %u bps\n", gd->baudrate);
-#endif
-}
-
 void __weak board_detail(void)
 {
 	/* Please define board_detail() for your PPC platform */
@@ -164,7 +155,7 @@ int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 	print_bi_mem(bd);
 	print_bi_flash(bd);
 	print_eth_ip_addr();
-	print_baudrate();
+	printf("baudrate    = %u bps\n", gd->baudrate);
 	print_bi_dram(bd);
 	print_num("relocaddr", gd->relocaddr);
 	print_num("reloc off", gd->reloc_off);
