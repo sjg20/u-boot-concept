e3144bf41ee3eb8ab44d20cc7af2f9d63d3e7658
Simon Glass <sjg@chromium.org>
2020-06-20 11:00:24 -0600

    wip
    Change-Id: Id97412a45cd209ea4060eccded6ba45d32614bf2
    
    wip
    
    Change-Id: I1bb6dbb7094710f004c9936a21ea7ebdb76bcdb7
    
    fix
    
    Change-Id: If519cb11273e54bcf2cd471583a727f53a9e86ee
    
    wip
    
    Change-Id: Id6d147bfb06c1b019845efdf4e56ffc7dd6a481d
    
    wip
    
    Change-Id: I366a58958b3db41a95b43037e6e037faebd73bc3

--- drivers/core/device.c
+++ drivers/core/device.c
@@ -270,7 +270,7 @@ int device_bind_by_name(struct udevice *parent, bool pre_reloc_only,
 	if (ret)
 		return ret;
 #if CONFIG_IS_ENABLED(OF_PLATDATA)
-// 	info->dev = *devp;
+	info->dev = *devp;
 #endif
 
 	return ret;
