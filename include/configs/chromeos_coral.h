0728830e6377fa977684f34270f00fd184ad69d0
Simon Glass <sjg@chromium.org>
2020-11-08 14:39:06 -0700

    wip
    Change-Id: I2aeaf41f90e76684e2afe44c493ed6858f52d1b1

--- include/configs/chromeos_coral.h
+++ include/configs/chromeos_coral.h
@@ -18,6 +18,19 @@
 
 #undef CONFIG_BOOTCOMMAND
 
-#define CONFIG_BOOTCOMMAND	"vboot go auto"
+#define CONFIG_BOOTCOMMAND2	"vboot go auto"
+
+#define xCONFIG_BOOTCOMMAND	\
+	"read mmc 2:2 100000 0 80; setexpr loader *001004f0; " \
+	"setexpr size *00100518; setexpr blocks $size / 200; " \
+	"read mmc 2:2 100000 80 $blocks; setexpr setup $loader - 1000; " \
+	"setexpr cmdline_ptr $loader - 2000; " \
+	"setexpr.s cmdline *$cmdline_ptr; " \
+	"setexpr cmdline gsub %U \\\\${uuid}; " \
+	"echo fred; " \
+	"if part uuid mmc 2:2 uuid; then " \
+	"zboot start 100000 0 0 0 $setup cmdline; " \
+	"zboot load; zboot setup; zboot dump; zboot go;" \
+	"fi"
 
 #endif	/* _OS_CONFIG_H */
