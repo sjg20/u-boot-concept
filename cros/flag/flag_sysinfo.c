sjg20/u-boot

cros: Add base implementation
Add an implementation of Chromium OS verified boot. This is entirely
contained within the cros/ subdrectory. It includes drivers, device-tree
files, commands and callbacks.

Change-Id: Icb4e2eb64116293d5ac1b63db8c771b03ec7bcc6

cros/flag/flag_sysinfo.c
@@ -1,0 +1,85 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * A vboot flag controlled by coreboot sysinfo tables (x86 only)
+ *
+ * Copyright 2018 Google LLC
+ */
+
+#define LOG_CATEGORY UCLASS_CROS_VBOOT_FLAG
+
+#include <common.h>
+#include <dm.h>
+#include <log.h>
+#include <cros/vboot_flag.h>
+
+/**
+ * Private data for this driver
+ *
+ * @name: Name of this flag
+ * @port: GPIO port number (coreboot value)
+ * @active_high: true if active hight, false if active low (inverted)
+ * @value: raw GPIO value as read by coreboot
+ */
+struct flag_sysinfo_priv {
+	const char *name;
+	int port;
+	bool active_high;
+	int value;
+};
+
+static int flag_sysinfo_read(struct udevice *dev)
+{
+	struct flag_sysinfo_priv *priv = dev_get_priv(dev);
+
+	return priv->value;
+}
+
+static int flag_sysinfo_probe(struct udevice *dev)
+{
+	struct flag_sysinfo_priv *priv = dev_get_priv(dev);
+	struct sysinfo_t *sysinfo;
+	const char *name;
+	int i;
+
+	name = dev_read_string(dev, "google,name");
+	if (!name) {
+		log_error("Missing flag name in '%s'\n", dev->name);
+		return -EINVAL;
+	}
+	priv->name = name;
+	sysinfo = lib_sysinfo_get();
+	for (i = 0; i < sysinfo->num_gpios; i++) {
+		if (strncmp((char *)sysinfo->gpios[i].name, name,
+			    GPIO_MAX_NAME_LENGTH))
+			continue;
+
+		/* Entry found */
+		priv->port = sysinfo->gpios[i].port;
+		priv->active_high = sysinfo->gpios[i].polarity;
+		priv->value = sysinfo->gpios[i].value;
+		if (!priv->active_high)
+			priv->value = !priv->value;
+
+		return 0;
+	}
+
+	return 0;
+}
+
+static const struct vboot_flag_ops flag_sysinfo_ops = {
+	.read	= flag_sysinfo_read,
+};
+
+static const struct udevice_id flag_sysinfo_ids[] = {
+	{ .compatible = "google,sysinfo-flag" },
+	{ }
+};
+
+U_BOOT_DRIVER(flag_sysinfo_drv) = {
+	.name		= "flag_sysinfo",
+	.id		= UCLASS_CROS_VBOOT_FLAG,
+	.of_match	= flag_sysinfo_ids,
+	.probe		= flag_sysinfo_probe,
+	.ops		= &flag_sysinfo_ops,
+	.priv_auto_alloc_size	= sizeof(struct flag_sysinfo_priv),
+};

cros/fwstore/fwstore_mmc.c
@@ -1,0 +1,239 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Implementation of firmware storage access interface for MMC
+ * NOTE THAT THIS CODE IS EFFECTIVELY UNTESTED and is for example only
+ *
+ * Copyright 2018 Google LLC
+ */
+
+#define LOG_CATEGORY UCLASS_CROS_FWSTORE
+
+#include <common.h>
+#include <dm.h>
+#include <malloc.h>
+#include <mmc.h>
+#include <cros/cros_common.h>
+#include <cros/cros_ofnode.h>
+#include <cros/fwstore.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+struct priv {
+	struct udevice *dev;
+	struct mmc *mmc;
+	u32 ro_section_size;	/* Offsets bigger are on partition 2 */
+	struct blk_desc *blk;
+};
+
+/*
+ * Determine which boot partition the access falls within.
+ *
+ * Return partition number, or -EINVAL if discontinuous
+ */
+static int check_partition(struct priv *priv, u32 offset, u32 count)
+{
+	int partition = 1;
+
+	/*
+	 * Check continuity
+	 * If it starts in partition 1, and ends in partition 2, things
+	 * will not go well.
+	 */
+	if (offset < priv->ro_section_size &&
+	    offset + count >= priv->ro_section_size) {
+		log_debug("Boot partition access not contiguous\n");
+		return -EINVAL;
+	}
+
+	/* Offsets not in the RO section must be in partition 2 */
+	if (offset >= priv->ro_section_size)
+		partition = 2;
+
+	return partition;
+}
+
+static int fwstore_mmc_read(struct udevice *dev, ulong offset, ulong count,
+			    void *buf)
+{
+	struct priv *priv = dev_get_priv(dev);
+	int partition, err;
+	u8 *tmp_buf;
+	int n, ret = -1;
+	int start_block, start_block_offset, end_block, total_blocks;
+
+	log_debug("offset=%#lx, count=%#lx\n", offset, count);
+	partition = check_partition(priv, offset, count);
+	if (partition < 0)
+		return -1;
+
+	if (partition == 2) {
+		log_debug("Reading from partition 2\n");
+		offset -= priv->ro_section_size;
+	}
+
+	start_block = offset / MMC_MAX_BLOCK_LEN;
+	start_block_offset = offset % MMC_MAX_BLOCK_LEN;
+	end_block = (offset + count) / MMC_MAX_BLOCK_LEN;
+
+	/* Read start to end, inclusive */
+	total_blocks = end_block - start_block + 1;
+
+	log_debug("Reading %d blocks\n", total_blocks);
+
+	tmp_buf = malloc(MMC_MAX_BLOCK_LEN * total_blocks);
+	if (!tmp_buf) {
+		log_debug("Failed to allocate buffer\n");
+		goto out;
+	}
+
+	/* Open partition */
+	err = mmc_hwpart_access(priv->mmc, partition);
+	if (err) {
+		log_debug("Failed to open boot partition %d\n", partition);
+		goto out_free;
+	}
+
+	/* Read data */
+	n = blk_dread(priv->blk, start_block, total_blocks, tmp_buf);
+	if (n != total_blocks) {
+		log_debug("Failed to read blocks\n");
+		goto out_close;
+	}
+
+	/* Copy to output buffer */
+	memcpy(buf, tmp_buf + start_block_offset, count);
+
+	ret = 0;
+
+out_close:
+	/* Close partition */
+	err = mmc_hwpart_access(priv->mmc, 0);
+	if (err) {
+		log_debug("Failed to close boot partition\n");
+		ret = -1;
+	}
+
+out_free:
+	free(tmp_buf);
+out:
+	return ret;
+}
+
+/*
+ * Does not support unaligned writes.
+ * Offset and count must be offset-aligned.
+ */
+static int fwstore_mmc_write(struct udevice *dev, ulong offset, ulong count,
+			     void *buf)
+{
+	struct priv *priv = dev_get_priv(dev);
+	u32 num, start_block, total_blocks;
+	int partition, err, ret = 0;
+
+	/* Writes not aligned to block size are unsupported */
+	if (offset % MMC_MAX_BLOCK_LEN) {
+		log_debug("Offset of %ld bytes not aligned to 512 byte boundary\n",
+			  offset);
+		return -1;
+	}
+
+	if (count % MMC_MAX_BLOCK_LEN) {
+		log_debug("Count of %ld bytes not aligned to 512 byte boundary\n",
+			  count);
+		return -1;
+	}
+
+	/* Determine partition */
+	partition = check_partition(priv, offset, count);
+	if (partition < 0)
+		return -1;
+
+	if (partition == 2) {
+		log_debug("Writing to partition 2\n");
+		offset -= priv->ro_section_size;
+	}
+
+	start_block = offset / MMC_MAX_BLOCK_LEN;
+	total_blocks = count / MMC_MAX_BLOCK_LEN;
+
+	/* Open partition */
+	err = mmc_hwpart_access(priv->mmc, partition);
+	if (err) {
+		log_debug("Failed to open boot partition %d\n", partition);
+		return -1;
+	}
+
+	/* Write data */
+	num = blk_dwrite(priv->blk, start_block, total_blocks, buf);
+	if (num != total_blocks) {
+		log_debug("Failed to write blocks\n");
+		ret = -1;
+		goto out;
+	}
+
+out:
+	/* Close partition */
+	err = mmc_hwpart_access(priv->mmc, 0);
+	if (err) {
+		log_debug("Failed to close boot partition\n");
+		return -1;
+	}
+
+	return ret;
+}
+
+static int fwstore_mmc_sw_wp_enabled_mmc(struct udevice *dev)
+{
+	struct priv *priv = dev_get_priv(dev);
+
+	return mmc_get_boot_wp(priv->mmc);
+}
+
+static int fwstore_mmc_probe(struct udevice *dev)
+{
+	struct priv *priv = dev_get_priv(dev);
+	struct ofnode_phandle_args args;
+	int ret;
+
+	log_debug("init %s\n", dev->name);
+	ret = dev_read_phandle_with_args(dev, "firmware-storage", NULL, 0, 0,
+					 &args);
+	if (ret < 0) {
+		log_debug("fail to look up phandle for device %s\n", dev->name);
+		return ret;
+	}
+
+	ret = uclass_get_device_by_ofnode(UCLASS_MMC, args.node, &priv->dev);
+	if (ret) {
+		log_debug("fail to init MMC at %s: %s: ret=%d\n",
+			  dev->name, ofnode_get_name(args.node), ret);
+		return ret;
+	}
+
+	/* TODO(sjg@chromium.org): Lookup partition size in binman table */
+	priv->ro_section_size = 2 << 20;
+	priv->mmc = mmc_get_mmc_dev(priv->dev);
+	priv->blk = blk_get_by_device(priv->dev);
+
+	return 0;
+}
+
+static const struct cros_fwstore_ops fwstore_mmc_ops = {
+	.read		= fwstore_mmc_read,
+	.write		= fwstore_mmc_write,
+	.sw_wp_enabled	= fwstore_mmc_sw_wp_enabled_mmc,
+};
+
+static struct udevice_id fwstore_mmc_ids[] = {
+	{ .compatible = "google,fwstore-mmc" },
+	{ },
+};
+
+U_BOOT_DRIVER(fwstore_mmc) = {
+	.name	= "fwstore_mmc",
+	.id	= UCLASS_CROS_FWSTORE,
+	.of_match = fwstore_mmc_ids,
+	.ops	= &fwstore_mmc_ops,
+	.probe	= fwstore_mmc_probe,
+	.priv_auto_alloc_size = sizeof(struct priv),
+};

cros/vboot/keyboard.c
@@ -1,0 +1,169 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Keyboard handling, including the implementation of VbExKeyboardRead() and
+ * remapping of keys for the Fully-Automated Firmware Test (FAFT).
+ *
+ * Copyright 2018 Google LLC
+ */
+
+#include <common.h>
+#include <gbb_header.h>
+#include <cros/keyboard.h>
+#include <cros/cros_ofnode.h>
+#include <cros/vboot.h>
+#include <dm/ofnode.h>
+
+/* Control Sequence Introducer for arrow keys */
+#define CSI_0		0x1B	/* Escape */
+#define CSI_1		0x5B	/* '[' */
+
+/* Types of keys to be overridden */
+enum key_type_t {
+	KEY_TYPE_ASCII,
+	KEY_TYPE_SPECIAL,
+
+	KEY_TYPE_COUNT,
+};
+
+/* Each fdt key array holds three pairs of keys */
+#define KEY_ARRAY_SIZE	(2 * 3)
+
+/**
+ * struct remap_key - Keys to remap
+ *
+ * @array: List of keys to remap, as pairs:
+ *		- keycode to remap
+ *		- new keycode to use
+ * @array_ptr: pointes to @array if it is initialised, elseNULL
+ */
+struct remap_key {
+	u32 array[KEY_ARRAY_SIZE];
+	u32 *array_ptr;
+};
+
+static struct remap_key remap_keys[KEY_TYPE_COUNT];
+
+int vboot_keymap_init(struct vboot_info *vboot)
+{
+	ofnode node = vboot->config;
+
+	if (!ofnode_valid(node))
+		return -ENOENT;
+
+	if (!ofnode_read_u32_array(node, "faft-key-remap-special",
+				   remap_keys[KEY_TYPE_SPECIAL].array,
+				   KEY_ARRAY_SIZE)) {
+		remap_keys[KEY_TYPE_SPECIAL].array_ptr =
+			remap_keys[KEY_TYPE_SPECIAL].array;
+	}
+
+	if (!ofnode_read_u32_array(node, "faft-key-remap-ascii",
+				   remap_keys[KEY_TYPE_ASCII].array,
+				   KEY_ARRAY_SIZE)) {
+		remap_keys[KEY_TYPE_ASCII].array_ptr =
+			remap_keys[KEY_TYPE_ASCII].array;
+	}
+
+	return 0;
+}
+
+/**
+ * Replace normal ascii keys and special keys if the mainboard
+ * fdt has either an ascii or a special key remap array.
+ *
+ * @keyp:	pointer to key to be checked on entry; on exit the value of the
+ *		remapped key on success, or the original key on failure
+ * @keytype:	KEY_TYPE_ASCII or KEY_TYPE_SPECIAL
+ *
+ * @return	0 on success, -EPERM if overriding is not enabled, -ENOENT if
+ *		no override found
+ */
+static int faft_key_remap(int *keyp, enum key_type_t keytype)
+{
+	struct vboot_info *vboot = vboot_get();
+	u32 gbb_flags = vboot_get_gbb_flags(vboot);
+	int i;
+
+	if ((gbb_flags & GBB_FLAG_FAFT_KEY_OVERIDE) == 0)
+		return -1;
+
+	if (remap_keys[keytype].array_ptr) {
+		for (i = 0; i < KEY_ARRAY_SIZE; i += 2) {
+			if (*keyp == remap_keys[keytype].array_ptr[i]) {
+				*keyp = remap_keys[keytype].array_ptr[i + 1];
+				return 0;
+			}
+		}
+	}
+
+	return -ENOENT;
+}
+
+u32 VbExKeyboardRead(void)
+{
+	int ch = 0;
+
+	/* No input available */
+	if (!tstc())
+		goto out;
+
+	/* Read a non-Escape character or a standalone Escape character */
+	ch = getc();
+	if (ch != CSI_0 || !tstc()) {
+		/* Handle normal asci keys for FAFT keyboard matrix */
+		if (faft_key_remap(&ch, KEY_TYPE_ASCII) >= 0)
+			goto out;
+
+		/*
+		 * Special handle of Ctrl-Enter, which is converted into '\n'
+		 * by i8042 driver.
+		 */
+		if (ch == '\n')
+			ch = VB_KEY_CTRL_ENTER;
+		goto out;
+	}
+
+	/* Filter out non- Escape-[ sequence */
+	if (getc() != CSI_1) {
+		ch = 0;
+		goto out;
+	}
+
+	/* Get special keys */
+	ch = getc();
+
+	/* Handle special keys for FAFT keyboard matrix */
+	if (faft_key_remap(&ch, KEY_TYPE_SPECIAL) >= 0)
+		goto out;
+
+	/* Special values for arrow up/down/left/right */
+	switch (ch) {
+	case 'A':
+		ch = VB_KEY_UP;
+		break;
+	case 'B':
+		ch = VB_KEY_DOWN;
+		break;
+	case 'C':
+		ch = VB_KEY_RIGHT;
+		break;
+	case 'D':
+		ch = VB_KEY_LEFT;
+		break;
+	default:
+		/* Filter out speical keys that we do not recognise */
+		ch = 0;
+		break;
+	}
+
+out:
+	return ch;
+}
+
+u32 VbExKeyboardReadWithFlags(u32 *flags_ptr)
+{
+	if (flags_ptr)
+		/* We trust keyboards on u-boot legacy devices */
+		*flags_ptr = VB_KEY_FLAG_TRUSTED_KEYBOARD;
+	return VbExKeyboardRead();
+}

sjg20/u-boot-concept

cros/flag/flag_sysinfo.c
@@ -1,0 +1,85 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * A vboot flag controlled by coreboot sysinfo tables (x86 only)
+ *
+ * Copyright 2018 Google LLC
+ */
+
+#define LOG_CATEGORY UCLASS_CROS_VBOOT_FLAG
+
+#include <common.h>
+#include <dm.h>
+#include <log.h>
+#include <cros/vboot_flag.h>
+
+/**
+ * Private data for this driver
+ *
+ * @name: Name of this flag
+ * @port: GPIO port number (coreboot value)
+ * @active_high: true if active hight, false if active low (inverted)
+ * @value: raw GPIO value as read by coreboot
+ */
+struct flag_sysinfo_priv {
+	const char *name;
+	int port;
+	bool active_high;
+	int value;
+};
+
+static int flag_sysinfo_read(struct udevice *dev)
+{
+	struct flag_sysinfo_priv *priv = dev_get_priv(dev);
+
+	return priv->value;
+}
+
+static int flag_sysinfo_probe(struct udevice *dev)
+{
+	struct flag_sysinfo_priv *priv = dev_get_priv(dev);
+	struct sysinfo_t *sysinfo;
+	const char *name;
+	int i;
+
+	name = dev_read_string(dev, "google,name");
+	if (!name) {
+		log_error("Missing flag name in '%s'\n", dev->name);
+		return -EINVAL;
+	}
+	priv->name = name;
+	sysinfo = lib_sysinfo_get();
+	for (i = 0; i < sysinfo->num_gpios; i++) {
+		if (strncmp((char *)sysinfo->gpios[i].name, name,
+			    GPIO_MAX_NAME_LENGTH))
+			continue;
+
+		/* Entry found */
+		priv->port = sysinfo->gpios[i].port;
+		priv->active_high = sysinfo->gpios[i].polarity;
+		priv->value = sysinfo->gpios[i].value;
+		if (!priv->active_high)
+			priv->value = !priv->value;
+
+		return 0;
+	}
+
+	return 0;
+}
+
+static const struct vboot_flag_ops flag_sysinfo_ops = {
+	.read	= flag_sysinfo_read,
+};
+
+static const struct udevice_id flag_sysinfo_ids[] = {
+	{ .compatible = "google,sysinfo-flag" },
+	{ }
+};
+
+U_BOOT_DRIVER(flag_sysinfo_drv) = {
+	.name		= "flag_sysinfo",
+	.id		= UCLASS_CROS_VBOOT_FLAG,
+	.of_match	= flag_sysinfo_ids,
+	.probe		= flag_sysinfo_probe,
+	.ops		= &flag_sysinfo_ops,
+	.priv_auto	= sizeof(struct flag_sysinfo_priv),
+};

cros/fwstore/fwstore_mmc.c
@@ -1,0 +1,240 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Implementation of firmware storage access interface for MMC
+ * NOTE THAT THIS CODE IS EFFECTIVELY UNTESTED and is for example only
+ *
+ * Copyright 2018 Google LLC
+ */
+
+#define LOG_CATEGORY UCLASS_CROS_FWSTORE
+
+#include <common.h>
+#include <dm.h>
+#include <log.h>
+#include <malloc.h>
+#include <mmc.h>
+#include <cros/cros_common.h>
+#include <cros/cros_ofnode.h>
+#include <cros/fwstore.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+struct priv {
+	struct udevice *dev;
+	struct mmc *mmc;
+	u32 ro_section_size;	/* Offsets bigger are on partition 2 */
+	struct blk_desc *blk;
+};
+
+/*
+ * Determine which boot partition the access falls within.
+ *
+ * Return partition number, or -EINVAL if discontinuous
+ */
+static int check_partition(struct priv *priv, u32 offset, u32 count)
+{
+	int partition = 1;
+
+	/*
+	 * Check continuity
+	 * If it starts in partition 1, and ends in partition 2, things
+	 * will not go well.
+	 */
+	if (offset < priv->ro_section_size &&
+	    offset + count >= priv->ro_section_size) {
+		log_debug("Boot partition access not contiguous\n");
+		return -EINVAL;
+	}
+
+	/* Offsets not in the RO section must be in partition 2 */
+	if (offset >= priv->ro_section_size)
+		partition = 2;
+
+	return partition;
+}
+
+static int fwstore_mmc_read(struct udevice *dev, ulong offset, ulong count,
+			    void *buf)
+{
+	struct priv *priv = dev_get_priv(dev);
+	int partition, err;
+	u8 *tmp_buf;
+	int n, ret = -1;
+	int start_block, start_block_offset, end_block, total_blocks;
+
+	log_debug("offset=%#lx, count=%#lx\n", offset, count);
+	partition = check_partition(priv, offset, count);
+	if (partition < 0)
+		return -1;
+
+	if (partition == 2) {
+		log_debug("Reading from partition 2\n");
+		offset -= priv->ro_section_size;
+	}
+
+	start_block = offset / MMC_MAX_BLOCK_LEN;
+	start_block_offset = offset % MMC_MAX_BLOCK_LEN;
+	end_block = (offset + count) / MMC_MAX_BLOCK_LEN;
+
+	/* Read start to end, inclusive */
+	total_blocks = end_block - start_block + 1;
+
+	log_debug("Reading %d blocks\n", total_blocks);
+
+	tmp_buf = malloc(MMC_MAX_BLOCK_LEN * total_blocks);
+	if (!tmp_buf) {
+		log_debug("Failed to allocate buffer\n");
+		goto out;
+	}
+
+	/* Open partition */
+	err = mmc_hwpart_access(priv->mmc, partition);
+	if (err) {
+		log_debug("Failed to open boot partition %d\n", partition);
+		goto out_free;
+	}
+
+	/* Read data */
+	n = blk_dread(priv->blk, start_block, total_blocks, tmp_buf);
+	if (n != total_blocks) {
+		log_debug("Failed to read blocks\n");
+		goto out_close;
+	}
+
+	/* Copy to output buffer */
+	memcpy(buf, tmp_buf + start_block_offset, count);
+
+	ret = 0;
+
+out_close:
+	/* Close partition */
+	err = mmc_hwpart_access(priv->mmc, 0);
+	if (err) {
+		log_debug("Failed to close boot partition\n");
+		ret = -1;
+	}
+
+out_free:
+	free(tmp_buf);
+out:
+	return ret;
+}
+
+/*
+ * Does not support unaligned writes.
+ * Offset and count must be offset-aligned.
+ */
+static int fwstore_mmc_write(struct udevice *dev, ulong offset, ulong count,
+			     void *buf)
+{
+	struct priv *priv = dev_get_priv(dev);
+	u32 num, start_block, total_blocks;
+	int partition, err, ret = 0;
+
+	/* Writes not aligned to block size are unsupported */
+	if (offset % MMC_MAX_BLOCK_LEN) {
+		log_debug("Offset of %ld bytes not aligned to 512 byte boundary\n",
+			  offset);
+		return -1;
+	}
+
+	if (count % MMC_MAX_BLOCK_LEN) {
+		log_debug("Count of %ld bytes not aligned to 512 byte boundary\n",
+			  count);
+		return -1;
+	}
+
+	/* Determine partition */
+	partition = check_partition(priv, offset, count);
+	if (partition < 0)
+		return -1;
+
+	if (partition == 2) {
+		log_debug("Writing to partition 2\n");
+		offset -= priv->ro_section_size;
+	}
+
+	start_block = offset / MMC_MAX_BLOCK_LEN;
+	total_blocks = count / MMC_MAX_BLOCK_LEN;
+
+	/* Open partition */
+	err = mmc_hwpart_access(priv->mmc, partition);
+	if (err) {
+		log_debug("Failed to open boot partition %d\n", partition);
+		return -1;
+	}
+
+	/* Write data */
+	num = blk_dwrite(priv->blk, start_block, total_blocks, buf);
+	if (num != total_blocks) {
+		log_debug("Failed to write blocks\n");
+		ret = -1;
+		goto out;
+	}
+
+out:
+	/* Close partition */
+	err = mmc_hwpart_access(priv->mmc, 0);
+	if (err) {
+		log_debug("Failed to close boot partition\n");
+		return -1;
+	}
+
+	return ret;
+}
+
+static int fwstore_mmc_sw_wp_enabled_mmc(struct udevice *dev)
+{
+	struct priv *priv = dev_get_priv(dev);
+
+	return mmc_get_boot_wp(priv->mmc);
+}
+
+static int fwstore_mmc_probe(struct udevice *dev)
+{
+	struct priv *priv = dev_get_priv(dev);
+	struct ofnode_phandle_args args;
+	int ret;
+
+	log_debug("init %s\n", dev->name);
+	ret = dev_read_phandle_with_args(dev, "firmware-storage", NULL, 0, 0,
+					 &args);
+	if (ret < 0) {
+		log_debug("fail to look up phandle for device %s\n", dev->name);
+		return ret;
+	}
+
+	ret = uclass_get_device_by_ofnode(UCLASS_MMC, args.node, &priv->dev);
+	if (ret) {
+		log_debug("fail to init MMC at %s: %s: ret=%d\n",
+			  dev->name, ofnode_get_name(args.node), ret);
+		return ret;
+	}
+
+	/* TODO(sjg@chromium.org): Lookup partition size in binman table */
+	priv->ro_section_size = 2 << 20;
+	priv->mmc = mmc_get_mmc_dev(priv->dev);
+	priv->blk = blk_get_by_device(priv->dev);
+
+	return 0;
+}
+
+static const struct cros_fwstore_ops fwstore_mmc_ops = {
+	.read		= fwstore_mmc_read,
+	.write		= fwstore_mmc_write,
+	.sw_wp_enabled	= fwstore_mmc_sw_wp_enabled_mmc,
+};
+
+static struct udevice_id fwstore_mmc_ids[] = {
+	{ .compatible = "google,fwstore-mmc" },
+	{ },
+};
+
+U_BOOT_DRIVER(fwstore_mmc) = {
+	.name	= "fwstore_mmc",
+	.id	= UCLASS_CROS_FWSTORE,
+	.of_match = fwstore_mmc_ids,
+	.ops	= &fwstore_mmc_ops,
+	.probe	= fwstore_mmc_probe,
+	.priv_auto = sizeof(struct priv),
+};

cros/vboot/keyboard.c
@@ -1,0 +1,169 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Keyboard handling, including the implementation of VbExKeyboardRead() and
+ * remapping of keys for the Fully-Automated Firmware Test (FAFT).
+ *
+ * Copyright 2018 Google LLC
+ */
+
+#include <common.h>
+#include <gbb_header.h>
+#include <cros/keyboard.h>
+#include <cros/cros_ofnode.h>
+#include <cros/vboot.h>
+#include <dm/ofnode.h>
+
+/* Control Sequence Introducer for arrow keys */
+#define CSI_0		0x1B	/* Escape */
+#define CSI_1		0x5B	/* '[' */
+
+/* Types of keys to be overridden */
+enum key_type_t {
+	KEY_TYPE_ASCII,
+	KEY_TYPE_SPECIAL,
+
+	KEY_TYPE_COUNT,
+};
+
+/* Each fdt key array holds three pairs of keys */
+#define KEY_ARRAY_SIZE	(2 * 3)
+
+/**
+ * struct remap_key - Keys to remap
+ *
+ * @array: List of keys to remap, as pairs:
+ *		- keycode to remap
+ *		- new keycode to use
+ * @array_ptr: pointes to @array if it is initialised, elseNULL
+ */
+struct remap_key {
+	u32 array[KEY_ARRAY_SIZE];
+	u32 *array_ptr;
+};
+
+static struct remap_key remap_keys[KEY_TYPE_COUNT];
+
+int vboot_keymap_init(struct vboot_info *vboot)
+{
+	ofnode node = vboot->config;
+
+	if (!ofnode_valid(node))
+		return -ENOENT;
+
+	if (!ofnode_read_u32_array(node, "faft-key-remap-special",
+				   remap_keys[KEY_TYPE_SPECIAL].array,
+				   KEY_ARRAY_SIZE)) {
+		remap_keys[KEY_TYPE_SPECIAL].array_ptr =
+			remap_keys[KEY_TYPE_SPECIAL].array;
+	}
+
+	if (!ofnode_read_u32_array(node, "faft-key-remap-ascii",
+				   remap_keys[KEY_TYPE_ASCII].array,
+				   KEY_ARRAY_SIZE)) {
+		remap_keys[KEY_TYPE_ASCII].array_ptr =
+			remap_keys[KEY_TYPE_ASCII].array;
+	}
+
+	return 0;
+}
+
+/**
+ * Replace normal ascii keys and special keys if the mainboard
+ * fdt has either an ascii or a special key remap array.
+ *
+ * @keyp:	pointer to key to be checked on entry; on exit the value of the
+ *		remapped key on success, or the original key on failure
+ * @keytype:	KEY_TYPE_ASCII or KEY_TYPE_SPECIAL
+ *
+ * @return	0 on success, -EPERM if overriding is not enabled, -ENOENT if
+ *		no override found
+ */
+static int faft_key_remap(int *keyp, enum key_type_t keytype)
+{
+	struct vboot_info *vboot = vboot_get();
+	u32 gbb_flags = vboot_get_gbb_flags(vboot);
+	int i;
+
+	if ((gbb_flags & GBB_FLAG_FAFT_KEY_OVERIDE) == 0)
+		return -1;
+
+	if (remap_keys[keytype].array_ptr) {
+		for (i = 0; i < KEY_ARRAY_SIZE; i += 2) {
+			if (*keyp == remap_keys[keytype].array_ptr[i]) {
+				*keyp = remap_keys[keytype].array_ptr[i + 1];
+				return 0;
+			}
+		}
+	}
+
+	return -ENOENT;
+}
+
+u32 VbExKeyboardRead(void)
+{
+	int ch = 0;
+
+	/* No input available */
+	if (!tstc())
+		goto out;
+
+	/* Read a non-Escape character or a standalone Escape character */
+	ch = getchar();
+	if (ch != CSI_0 || !tstc()) {
+		/* Handle normal asci keys for FAFT keyboard matrix */
+		if (faft_key_remap(&ch, KEY_TYPE_ASCII) >= 0)
+			goto out;
+
+		/*
+		 * Special handle of Ctrl-Enter, which is converted into '\n'
+		 * by i8042 driver.
+		 */
+		if (ch == '\n')
+			ch = VB_KEY_CTRL_ENTER;
+		goto out;
+	}
+
+	/* Filter out non- Escape-[ sequence */
+	if (getchar() != CSI_1) {
+		ch = 0;
+		goto out;
+	}
+
+	/* Get special keys */
+	ch = getchar();
+
+	/* Handle special keys for FAFT keyboard matrix */
+	if (faft_key_remap(&ch, KEY_TYPE_SPECIAL) >= 0)
+		goto out;
+
+	/* Special values for arrow up/down/left/right */
+	switch (ch) {
+	case 'A':
+		ch = VB_KEY_UP;
+		break;
+	case 'B':
+		ch = VB_KEY_DOWN;
+		break;
+	case 'C':
+		ch = VB_KEY_RIGHT;
+		break;
+	case 'D':
+		ch = VB_KEY_LEFT;
+		break;
+	default:
+		/* Filter out speical keys that we do not recognise */
+		ch = 0;
+		break;
+	}
+
+out:
+	return ch;
+}
+
+u32 VbExKeyboardReadWithFlags(u32 *flags_ptr)
+{
+	if (flags_ptr)
+		/* We trust keyboards on u-boot legacy devices */
+		*flags_ptr = VB_KEY_FLAG_TRUSTED_KEYBOARD;
+	return VbExKeyboardRead();
+}