113c70af833767ef85d78068ad676b9d4ccfaeaf
Marek Vasut <marek.vasut+renesas@gmail.com>
2021-07-04 17:27:27 +0200

    ARM: rmobile: Align CPU: print with other prints
    The CPU: print only has one space after it, while the other prints
    from U-Boot align the value to offset 7. Align the CPU: print too.
    No functional change.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>

--- arch/arm/mach-rmobile/cpu_info.c
+++ arch/arm/mach-rmobile/cpu_info.c
@@ -109,12 +109,12 @@ int print_cpuinfo(void)
 	if (rmobile_cpuinfo[i].cpu_type == RMOBILE_CPU_TYPE_R8A7796) {
 		if ((rmobile_get_cpu_rev_integer() == 1) &&
 		    (rmobile_get_cpu_rev_fraction() == 1)) {
-			printf("CPU: Renesas Electronics R8A7796 rev 1.1/rev 1.2\n");
+			printf("CPU:   Renesas Electronics R8A7796 rev 1.1/rev 1.2\n");
 			return 0;
 		}
 	}
 
-	printf("CPU: Renesas Electronics %s rev %d.%d\n",
+	printf("CPU:   Renesas Electronics %s rev %d.%d\n",
 		rmobile_cpuinfo[i].cpu_name, rmobile_get_cpu_rev_integer(),
 		rmobile_get_cpu_rev_fraction());
 
