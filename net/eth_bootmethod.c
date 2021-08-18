381b2271cc136e9829f1a58ff6085c6e3936f8c6
Simon Glass <sjg@chromium.org>
2021-08-17 21:30:19 -0600

    ethernet: Add a bootmethod
    Add a bootmethod for Ethernet. It uses the distro boot mechanism to
    locate a file.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: Ibb454281fc9a82c3ad084f0f2f876e46809eee02

---
+++ net/eth_bootmethod.c
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Bootmethod for ethernet
+ *
+ * Copyright 2021 Google LLC
+ * Written by Simon Glass <sjg@chromium.org>
+ */
+
+#include <common.h>
+#include <bootmethod.h>
+#include <command.h>
+#include <distro.h>
+#include <dm.h>
+#include <net.h>
+
+static int eth_get_bootflow(struct udevice *dev, int seq,
+			    struct bootflow *bflow)
+{
+	char name[60];
+	int ret;
+
+	/*
+	 * Like distro boot, this assumes there is only one Ethernet device.
+	 * In this case, that means that @eth is ignored
+	 */
+	if (seq)
+		return log_msg_ret("dhcp", -ESHUTDOWN);
+
+	bflow->seq = seq;
+	snprintf(name, sizeof(name), "%s.%d", dev->name, seq);
+	bflow->name = strdup(name);
+	if (!bflow->name)
+		return log_msg_ret("name", -ENOMEM);
+	if (!bflow->name)
+		return log_msg_ret("name", -ENOMEM);
+	bflow->state = BOOTFLOWST_BASE;
+
+	/*
+	 * There is not a direct interface to the network stack so run
+	 * everything through the command-line interpreter for now.
+	 *
+	 * Don't bother checking the result of dhcp. It can fail with:
+	 *
+	 * DHCP client bound to address 192.168.4.50 (4 ms)
+	 * *** Warning: no boot file name; using 'C0A80432.img'
+	 * Using smsc95xx_eth device
+	 * TFTP from server 192.168.4.1; our IP address is 192.168.4.50
+	 * Filename 'C0A80432.img'.
+	 * Load address: 0x200000
+	 * Loading: *
+	 * TFTP error: 'File not found' (1)
+	 *
+	 * This is not a real failure, since we don't actually care if the
+	 * boot file exists.
+	 */
+	run_command("dhcp", 0);
+	bflow->state = BOOTFLOWST_MEDIA;
+
+	if (CONFIG_IS_ENABLED(BOOTMETHOD_DISTRO)) {
+		ret = distro_net_setup(bflow);
+		if (ret)
+			return log_msg_ret("distro", ret);
+	}
+
+	return 0;
+}
+
+struct bootmethod_ops eth_bootmethod_ops = {
+	.get_bootflow	= eth_get_bootflow,
+};
+
+U_BOOT_DRIVER(eth_bootmethod) = {
+	.name		= "eth_bootmethod",
+	.id		= UCLASS_BOOTMETHOD,
+	.ops		= &eth_bootmethod_ops,
+};
