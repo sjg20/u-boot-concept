73ead4f4b48eafe26775e72251790d1c56656c65
Simon Glass <sjg@chromium.org>
2021-08-17 21:21:55 -0600

    bootmethod: Add a command
    Add a 'bootmethod' command to handle listing and selection of bootmethods.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: Ic1c0b48869db2404f5d8c89518eb88156b8bc46a

---
+++ cmd/bootmethod.c
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * 'bootmethod' command
+ *
+ * Copyright 2021 Google LLC
+ * Written by Simon Glass <sjg@chromium.org>
+ */
+
+#include <common.h>
+#include <bootmethod.h>
+#include <command.h>
+#include <dm.h>
+#include <dm/device-internal.h>
+#include <dm/uclass-internal.h>
+
+static int bootmethod_check_state(struct bootflow_state **statep)
+{
+	struct bootflow_state *state;
+	int ret;
+
+	ret = bootmethod_get_state(&state);
+	if (ret)
+		return ret;
+	if (!state->cur_bootmethod) {
+		printf("Please use 'bootmethod select' first\n");
+		return -ENOENT;
+	}
+	*statep = state;
+
+	return 0;
+}
+
+static int do_bootmethod_list(struct cmd_tbl *cmdtp, int flag, int argc,
+			      char *const argv[])
+{
+	bool probe;
+
+	probe = argc >= 2 && !strcmp(argv[1], "-p");
+	bootmethod_list(probe);
+
+	return 0;
+}
+
+static int do_bootmethod_select(struct cmd_tbl *cmdtp, int flag, int argc,
+				char *const argv[])
+{
+	struct bootflow_state *state;
+	struct udevice *dev;
+	const char *name;
+	char *endp;
+	int seq;
+	int ret;
+
+	ret = bootmethod_get_state(&state);
+	if (ret)
+		return CMD_RET_FAILURE;
+	if (argc < 2) {
+		state->cur_bootmethod = NULL;
+		return 0;
+	}
+	name = argv[1];
+	seq = simple_strtol(name, &endp, 16);
+
+	/* Select by name or number */
+	if (*endp)
+		ret = uclass_get_device_by_name(UCLASS_BOOTMETHOD, name, &dev);
+	else
+		ret = uclass_get_device_by_seq(UCLASS_BOOTMETHOD, seq, &dev);
+	if (ret) {
+		printf("Cannot find '%s' (err=%d)\n", name, ret);
+		return CMD_RET_FAILURE;
+	}
+	state->cur_bootmethod = dev;
+
+	return 0;
+}
+
+static int do_bootmethod_info(struct cmd_tbl *cmdtp, int flag, int argc,
+			      char *const argv[])
+{
+	struct bootflow_state *state;
+	struct bootflow *bflow;
+	int ret, i, num_valid;
+	struct udevice *dev;
+	bool probe;
+
+	probe = argc >= 2 && !strcmp(argv[1], "-p");
+
+	ret = bootmethod_check_state(&state);
+	if (ret)
+		return CMD_RET_FAILURE;
+
+	dev = state->cur_bootmethod;
+
+	/* Count the number of bootflows, including how many are valid*/
+	num_valid = 0;
+	for (ret = bootmethod_first_bootflow(dev, &bflow), i = 0;
+	     !ret;
+	     ret = bootmethod_next_bootflow(&bflow), i++)
+		num_valid += bflow->state == BOOTFLOWST_LOADED;
+
+	/*
+	 * Prove the device, if requested, otherwise assume that there is no
+	 * error
+	 */
+	ret = 0;
+	if (probe)
+		ret = device_probe(dev);
+
+	printf("Name:      %s\n", dev->name);
+	printf("Sequence:  %d\n", dev_seq(dev));
+	printf("Status:    %s\n", ret ? simple_itoa(ret) : device_active(dev) ?
+		"Probed" : "OK");
+	printf("Uclass:    %s\n", dev_get_uclass_name(dev_get_parent(dev)));
+	printf("Bootflows: %d (%d valid)\n", i, num_valid);
+
+	return 0;
+}
+
+#ifdef CONFIG_SYS_LONGHELP
+static char bootmethod_help_text[] =
+	"list [-p]      - list all available bootmethods (-p to probe)\n"
+	"bootmethod select <bm>    - select a bootmethod by name\n"
+	"bootmethod info [-p]      - show information about a bootmethod (-p to probe)";
+#endif
+
+U_BOOT_CMD_WITH_SUBCMDS(bootmethod, "Bootmethods", bootmethod_help_text,
+	U_BOOT_SUBCMD_MKENT(list, 2, 1, do_bootmethod_list),
+	U_BOOT_SUBCMD_MKENT(select, 2, 1, do_bootmethod_select),
+	U_BOOT_SUBCMD_MKENT(info, 2, 1, do_bootmethod_info));
