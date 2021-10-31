1699dde7961fe2b541fee0cafb921d9f9069a7f0
Simon Glass <sjg@chromium.org>
2021-10-31 10:58:14 -0600

    passage: Add docs for spl_handoff
    This tag already exists in U-Boot. Add documentation, following the
    format set out.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/handoff.h
+++ include/handoff.h
@@ -9,16 +9,20 @@
 #define __HANDOFF_H
 
 #if CONFIG_IS_ENABLED(HANDOFF)
-
 #include <asm/handoff.h>
+#endif
 
 /**
  * struct spl_handoff - information passed from SPL to U-Boot proper
  *
+ * bloblist_tag: BLOBLISTT_U_BOOT_SPL_HANDOFF
+ *
  * @ram_size: Value to use for gd->ram_size
  */
 struct spl_handoff {
+#if CONFIG_IS_ENABLED(HANDOFF)
 	struct arch_spl_handoff arch;
+#endif
 	u64 ram_size;
 	struct {
 		u64 start;
@@ -41,7 +45,5 @@ void handoff_load_dram_banks(struct spl_handoff *ho);
  * @return 0 if OK, -ve on error
  */
 int handoff_arch_save(struct spl_handoff *ho);
-
-#endif
 
 #endif
