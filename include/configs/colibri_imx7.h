0956a57ab9d5585f6236fa4ba3d0b84cc4b6c9fb
Simon Glass <sjg@chromium.org>
2021-11-17 18:02:11 -0700

    video: Convert CONFIG_VIDEO_LOGO to Kconfig
    This converts the following to Kconfig:
       CONFIG_VIDEO_LOGO
    
    Note that this option depends on CONFIG_DM_VIDEO now, since cfb_console is
    deprecated. The only relevant code is now in splash.c
    
    Drop the check for DM_VIDEO in that file.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/configs/colibri_imx7.h
+++ include/configs/colibri_imx7.h
@@ -199,7 +199,6 @@
 #define CONFIG_USBD_HS
 
 #if defined(CONFIG_DM_VIDEO)
-#define CONFIG_VIDEO_LOGO
 #define CONFIG_VIDEO_BMP_LOGO
 #endif
 
--- include/configs/mx53loco.h
+++ include/configs/mx53loco.h
@@ -146,6 +146,5 @@
 #endif
 
 /* Framebuffer and LCD */
-#define CONFIG_VIDEO_LOGO
 
 #endif				/* __CONFIG_H */
