sjg20/u-boot-concept

commit 0269d97649e6589809951f0360fb3b74f967329c

fastboot: implement current-slot
call cmd to know slot suffix if board support it.

Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>
(cherry picked from commit 731acda4499404acb3ea76d46f5b5c46c44cac29)
Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

drivers/fastboot/fb_getvar.c
@@ -177,8 +177,18 @@ static void getvar_platform(char *var_parameter, char *response)
 
 static void getvar_current_slot(char *var_parameter, char *response)
 {
-	/* A/B not implemented, for now always return "a" */
-	fastboot_okay("a", response);
+	const char *c = env_get("get_current_slot");
+	const char *slot;
+
+	if (c)
+		if (run_command(c, 0))
+			fastboot_fail("Cannot get_current_slot", response);
+		else {
+			slot = env_get("current_slot");
+			fastboot_okay(slot, response);
+		}
+	else
+		fastboot_fail("get_current_slot not set", response);
 }
 
 #if CONFIG_IS_ENABLED(FASTBOOT_FLASH)

commit 0d1f02044082cb38db1a70ee488625de93335e94

fastboot: fix warning
Fix the following warning:

drivers/fastboot/fb_getvar.c:184:21: warning: implicit declaration of function ‘run_command’ [-Wimplicit-function-declaration]
                   if (run_command(c, 0))
                       ^~~~~~~~~~~

Fixes: 0269d97649e6 ("fastboot: implement current-slot")
Signed-off-by: Fabien Parent <fparent@baylibre.com>

drivers/fastboot/fb_getvar.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2016 The Android Open Source Project
  */
 
+#include <command.h>
 #include <common.h>
 #include <env.h>
 #include <fastboot.h>