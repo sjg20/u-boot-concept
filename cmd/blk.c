f41ce1fcd043da92bf35ebcf144328f01e49a338
Simon Glass <sjg@chromium.org>
2021-12-15 19:48:17 -0700

    cmd: Add a blk command
    Add a command to list block devices, which are common to many other device
    types, such as USB, SCSI and MMC. This allow showing the standard
    underlying block device in a generic manner.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ cmd/blk.c
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * 'blk' command
+ *
+ * Copyright 2021 Google LLC
+ * Written by Simon Glass <sjg@chromium.org>
+ */
+
+#include <common.h>
+#include <command.h>
+#include <dm.h>
+#include <part.h>
+
+static int do_blk_list(struct cmd_tbl *cmdtp, int flag, int argc,
+		       char *const argv[])
+{
+	struct udevice *dev;
+	struct uclass *uc;
+
+	uclass_id_foreach_dev(UCLASS_BLK, dev, uc) {
+		const struct udevice *media = dev_get_parent(dev);
+		struct blk_desc *desc = dev_get_uclass_plat(dev);
+
+		printf("Type %s, parent %s, device %d: ",
+		       dev_get_uclass_name(media), media->name,
+		       dev_seq(dev->parent));
+		dev_print(desc);
+	}
+
+	return 0;
+}
+
+#ifdef CONFIG_SYS_LONGHELP
+static char blk_help_text[] =
+	"list      - list all block devices";
+#endif
+
+U_BOOT_CMD_WITH_SUBCMDS(blk, "Block devices", blk_help_text,
+	U_BOOT_SUBCMD_MKENT(list, 2, 1, do_blk_list));
