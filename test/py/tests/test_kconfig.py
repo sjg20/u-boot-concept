06245bce174c68151fdba1893f92b0e895677ad9
Simon Glass <sjg@chromium.org>
2022-07-31 13:37:22 -0600

    wip

--- test/py/tests/test_kconfig.py
+++ test/py/tests/test_kconfig.py
@@ -6,6 +6,9 @@ import pytest
 
 import u_boot_utils as util
 
+# This is needed for Azure, since the default '..' directory is not writeable
+TMPDIR = '/tmp/test_kconfig'
+
 @pytest.mark.slow
 def test_kconfig(u_boot_console):
     """Test build failures when IF_ENABLED_INT() option is not enabled"""
@@ -13,8 +16,8 @@ def test_kconfig(u_boot_console):
 
     # This detects build errors in test/lib/kconfig.c
     out = util.run_and_log(
-        cons, ['./tools/buildman/buildman', '--board', 'sandbox',
-               '-a', 'TEST_KCONFIG'], ignore_errors=True)
+        cons, ['./tools/buildman/buildman', '-m', '--board', 'sandbox',
+               '-a', 'TEST_KCONFIG', '-o', TMPDIR], ignore_errors=True)
     assert 'invalid_use_of_IF_ENABLED_INT' in out
     assert 'invalid_use_of_CONFIG_IF_ENABLED_INT' in out
 
@@ -25,8 +28,8 @@ def test_kconfig_spl(u_boot_console):
 
     # This detects build errors in test/lib/kconfig_spl.c
     out = util.run_and_log(
-        cons, ['./tools/buildman/buildman', '--board', 'sandbox_spl',
-               '-a', 'TEST_KCONFIG'], ignore_errors=True)
+        cons, ['./tools/buildman/buildman', '-m', '--board', 'sandbox_spl',
+               '-a', 'TEST_KCONFIG', '-o', TMPDIR], ignore_errors=True)
     assert 'invalid_use_of_IF_ENABLED_INT' in out
 
     # There is no CONFIG_SPL_TEST_KCONFIG, so the CONFIG_IF_ENABLED_INT()
