d11867362af88bacc10dc69d62e179d94b4d159a
Simon Glass <sjg@chromium.org>
2022-08-05 21:07:43 -0600

    blk: Switch over to using uclass IDs
    We currently have an if_type (interface type) and a uclass id. These are
    closely related and we don't need to have both.
    
    Drop the if_type values and use the uclass ones instead.
    
    Maintain the existing, subtle, one-way conversion between UCLASS_USB and
    UCLASS_MASS_STORAGE for now, and add a comment.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- cmd/bcb.c
+++ cmd/bcb.c
@@ -121,7 +121,7 @@ static int __bcb_load(int devnum, const char *partp)
 	char *endp;
 	int part, ret;
 
-	desc = blk_get_devnum_by_type(IF_TYPE_MMC, devnum);
+	desc = blk_get_devnum_by_type(UCLASS_MMC, devnum);
 	if (!desc) {
 		ret = -ENODEV;
 		goto err_read_fail;
@@ -286,7 +286,7 @@ static int __bcb_store(void)
 	u64 cnt;
 	int ret;
 
-	desc = blk_get_devnum_by_type(IF_TYPE_MMC, bcb_dev);
+	desc = blk_get_devnum_by_type(UCLASS_MMC, bcb_dev);
 	if (!desc) {
 		ret = -ENODEV;
 		goto err;
