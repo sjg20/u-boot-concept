15a73c50c8ee6224a36f66189ce8aa6a9c696796
Julien Masson <jmasson@baylibre.com>
2022-08-29 11:11:37 +0200

    clk: fix clk_get_rate() always return ulong
    According to clk_ops struct definition, the callback `get_rate()`
    return current clock rate value as ulong.
    `clk_get_rate()` should handle the clock rate returned as ulong also.
    
    Otherwise we may have invalid/truncated clock rate value returned by
    `clk_get_rate()`.
    
    `log_ret` has also been removed since it use an `int` in the macro
    definition.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- drivers/clk/clk-uclass.c
+++ drivers/clk/clk-uclass.c
@@ -490,7 +490,7 @@ int clk_free(struct clk *clk)
 ulong clk_get_rate(struct clk *clk)
 {
 	const struct clk_ops *ops;
-	int ret;
+	ulong ret;
 
 	debug("%s(clk=%p)\n", __func__, clk);
 	if (!clk_valid(clk))
@@ -500,11 +500,7 @@ ulong clk_get_rate(struct clk *clk)
 	if (!ops->get_rate)
 		return -ENOSYS;
 
-	ret = ops->get_rate(clk);
-	if (ret)
-		return log_ret(ret);
-
-	return 0;
+	return ops->get_rate(clk);
 }
 
 struct clk *clk_get_parent(struct clk *clk)
