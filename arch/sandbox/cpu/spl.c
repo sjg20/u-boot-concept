150cf0b539e4c60d1d8d67d1e673dc4c030b8e0a
Simon Glass <sjg@chromium.org>
2022-10-29 08:46:49 -0600

    test: Add a way to detect a test that breaks another
    When running unit tests, some may have side effects which cause a
    subsequent test to break. This can sometimes be seen when using 'ut dm'
    or similar.
    
    Add a new argument which allows a particular (failing) test to be run
    immediately after a certain number of tests have run. This allows the
    test causing the failure to be determined.
    
    Update the documentation also.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- arch/sandbox/cpu/spl.c
+++ arch/sandbox/cpu/spl.c
@@ -89,7 +89,7 @@ void spl_board_init(void)
 		int ret;
 
 		ret = ut_run_list("spl", NULL, tests, count,
-				  state->select_unittests, 1, false);
+				  state->select_unittests, 1, false, NULL);
 		/* continue execution into U-Boot */
 	}
 }
