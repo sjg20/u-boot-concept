u-boot/u-boot

commit a56f663f07073713042bb0fd08053aeb667e717b

vbe: Add info about the VBE device to the fwupd node
At present we put the driver in the /chosen node in U-Boot. This is a bit
strange, since U-Boot doesn't normally use that node itself. It is better
to put it under the bootstd node.

To make this work we need to copy create the node under /chosen when
fixing up the device tree. Copy over all the properties so that fwupd
knows what to do.

Update the sandbox device tree accordingly.

Signed-off-by: Simon Glass <sjg@chromium.org>

boot/vbe_simple_os.c
@@ -62,24 +62,34 @@ static int bootmeth_vbe_simple_ft_fixup(void *ctx, struct event *event)
 	 */
 	for (vbe_find_first_device(&dev); dev; vbe_find_next_device(&dev)) {
 		struct simple_state state;
-		ofnode node, subnode;
+		ofnode node, subnode, chosen;
 		int ret;
 
 		if (strcmp("vbe_simple", dev->driver->name))
 			continue;
 
-		/* Check if there is a node to fix up */
-		node = oftree_path(tree, "/chosen/fwupd");
-		if (!ofnode_valid(node))
+		/* Check if there is a node to fix up, adding if not */
+		chosen = oftree_path(tree, "/chosen");
+		if (!ofnode_valid(chosen))
 			continue;
-		subnode = ofnode_find_subnode(node, dev->name);
-		if (!ofnode_valid(subnode))
-			continue;
+		ret = ofnode_add_subnode(chosen, "fwupd", &node);
+		if (ret && ret != -EEXIST)
+			return log_msg_ret("fwu", ret);
 
-		log_debug("Fixing up: %s\n", dev->name);
+		ret = ofnode_add_subnode(node, dev->name, &subnode);
+		if (ret && ret != -EEXIST)
+			return log_msg_ret("dev", ret);
+
 		ret = device_probe(dev);
 		if (ret)
 			return log_msg_ret("probe", ret);
+
+		/* Copy over the vbe properties for fwupd */
+		log_debug("Fixing up: %s\n", dev->name);
+		ret = ofnode_copy_props(dev_ofnode(dev), subnode);
+		if (ret)
+			return log_msg_ret("cp", ret);
+
 		ret = vbe_simple_read_state(dev, &state);
 		if (ret)
 			return log_msg_ret("read", ret);

arch/sandbox/dts/test.dts
@@ -78,6 +78,7 @@
 	};
 
 	bootstd {
+		u-boot,dm-vpl;
 		compatible = "u-boot,boot-std";
 
 		filename-prefixes = "/", "/boot/";
@@ -89,6 +90,19 @@
 
 		efi {
 			compatible = "u-boot,distro-efi";
+		};
+
+		firmware0 {
+			u-boot,dm-vpl;
+			compatible = "fwupd,vbe-simple";
+			storage = "mmc1";
+			skip-offset = <0x200>;
+			area-start = <0x400>;
+			area-size = <0x1000>;
+			state-offset = <0x400>;
+			state-size = <0x40>;
+			version-offset = <0x800>;
+			version-size = <0x100>;
 		};
 	};
 
@@ -1403,21 +1417,6 @@
 		chosen-test {
 			compatible = "denx,u-boot-fdt-test";
 			reg = <9 1>;
-		};
-
-		fwupd {
-			compatible = "simple-bus";
-			firmware0 {
-				compatible = "fwupd,vbe-simple";
-				storage = "mmc1";
-				area-start = <0x400>;
-				area-size = <0x1000>;
-				skip-offset = <0x200>;
-				state-offset = <0x400>;
-				state-size = <0x40>;
-				version-offset = <0x800>;
-				version-size = <0x100>;
-			};
 		};
 	};
 

test/boot/vbe_simple.c
@@ -16,7 +16,12 @@
 #include <test/ut.h>
 #include "bootstd_common.h"
 
-/* Basic test of reading nvdata and updating a fwupd node in the device tree */
+/*
+ * Basic test of reading nvdata and updating a fwupd node in the device tree
+ *
+ * This sets up its own VBE info in the device, using bootstd_setup_for_tests()
+ * then does a VBE fixup and checks that everything is present.
+ */
 static int vbe_simple_test_base(struct unit_test_state *uts)
 {
 	const char *version, *bl_version;

sjg20/u-boot-concept

commit 39972827426af449c9e53323c718dfdcea977f54

vbe: Allow probing the VBE bootmeth to fail in OS fixup
This device is created when there are no bootmeths defined in the device
tree. But it cannot be probed without a device tree node.

For now, ignore a probe failure.

Series-version: 2
Series-changes: 2
- With 'with' typo
- Change to a debug message and add a comment

Series-to: u-boot
Series-cc: trini
Signed-off-by: Simon Glass <sjg@chromium.org>
Reported-by: Karsten Merker <merker@debian.org>
Suggested-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
Fixes: a56f663f0707 ("vbe: Add info about the VBE device to the fwupd node")

boot/vbe_simple_os.c
@@ -72,6 +72,18 @@ static int bootmeth_vbe_simple_ft_fixup(void *ctx, struct event *event)
 		chosen = oftree_path(tree, "/chosen");
 		if (!ofnode_valid(chosen))
 			continue;
+
+		ret = device_probe(dev);
+		if (ret) {
+			/*
+			 * This should become an error when VBE is updated to
+			 * only bind this device when a node exists
+			 */
+			log_debug("VBE device '%s' failed to probe (err=%d)",
+				  dev->name, ret);
+			return 0;
+		}
+
 		ret = ofnode_add_subnode(chosen, "fwupd", &node);
 		if (ret && ret != -EEXIST)
 			return log_msg_ret("fwu", ret);
@@ -79,10 +91,6 @@ static int bootmeth_vbe_simple_ft_fixup(void *ctx, struct event *event)
 		ret = ofnode_add_subnode(node, dev->name, &subnode);
 		if (ret && ret != -EEXIST)
 			return log_msg_ret("dev", ret);
-
-		ret = device_probe(dev);
-		if (ret)
-			return log_msg_ret("probe", ret);
 
 		/* Copy over the vbe properties for fwupd */
 		log_debug("Fixing up: %s\n", dev->name);