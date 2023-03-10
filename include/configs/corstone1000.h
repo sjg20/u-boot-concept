1045189fcec3a5d0ff25e2ea9fef751cbc080e84
Abdellatif El Khlifi <abdellatif.elkhlifi@arm.com>
2023-03-10 14:10:16 +0000

    arm_ffa: efi: corstone1000: enable MM communication
    turn on EFI MM communication
    
    On corstone1000 platform MM communication between u-boot
    and the secure world (Optee) is done using the FF-A bus.
    
    Signed-off-by: Abdellatif El Khlifi <abdellatif.elkhlifi@arm.com>
    Cc: Tom Rini <trini@konsulko.com>
    Cc: Simon Glass <sjg@chromium.org>
    Cc: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Cc: Jens Wiklander <jens.wiklander@linaro.org>

--- include/configs/corstone1000.h
+++ include/configs/corstone1000.h
@@ -1,9 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * (C) Copyright 2022 ARM Limited
  * (C) Copyright 2022 Linaro
  * Rui Miguel Silva <rui.silva@linaro.org>
- * Abdellatif El Khlifi <abdellatif.elkhlifi@arm.com>
+ * Copyright 2022-2023 Arm Limited and/or its affiliates <open-source-office@arm.com>
+ *
+ * Authors:
+ *   Abdellatif El Khlifi <abdellatif.elkhlifi@arm.com>
  *
  * Configuration for Corstone1000. Parts were derived from other ARM
  * configurations.
@@ -13,6 +15,15 @@
 #define __CORSTONE1000_H
 
 #include <linux/sizes.h>
+
+#define FFA_SHARED_MM_BUFFER_SIZE	SZ_4K /* 4 KB */
+
+/*
+ * shared buffer physical address used for communication between
+ * u-boot and the MM SP
+ */
+#define FFA_SHARED_MM_BUFFER_ADDR	0x02000000UL
+#define FFA_SHARED_MM_BUFFER_OFFSET	0
 
 #define V2M_BASE		0x80000000
 
