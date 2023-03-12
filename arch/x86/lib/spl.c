0530c1787506b3a50b4b81170b779e02a5a0c6b5
Simon Glass <sjg@chromium.org>
2023-03-12 11:27:44 -0600

    x86: Add on to existing MTRRs in SPL
    We don't actually set up all of these registers in SPL, so using
    mtrr_commit() with erase some. Use mtrr_set_next_var() instead.
    
    Series-to: u-boot
    Series-cc: bin
    Series-links: 345918
    Cover-letter:
    x86: Various fixes for chromebooks
    This adds some fixes for x86-based Chromebook builds which have picked up
    a few problems recently.
    
    With this, chromebook_link, chromebook_samus and chromebook_coral work
    correctly. For chromebook_link64 a problem remains where it fails to jump
    into 64-bit U-Boot.
    END
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- arch/x86/lib/spl.c
+++ arch/x86/lib/spl.c
@@ -147,14 +147,14 @@ static int x86_spl_init(void)
 	}
 
 	/* Cache the SPI flash. Otherwise copying the code to RAM takes ages */
-	ret = mtrr_add_request(MTRR_TYPE_WRBACK,
+	mtrr_set_next_var(MTRR_TYPE_WRBACK,
 			       (1ULL << 32) - CONFIG_XIP_ROM_SIZE,
 			       CONFIG_XIP_ROM_SIZE);
-	if (ret) {
-		debug("%s: SPI cache setup failed (err=%d)\n", __func__, ret);
-		return ret;
+	if (_LOG_DEBUG) {
+		ret = mtrr_list(mtrr_get_var_count(), MP_SELECT_BSP);
+		if (ret)
+			printf("mtrr_list failed\n");
 	}
-	mtrr_commit(true);
 # else
 	ret = syscon_get_by_driver_data(X86_SYSCON_PUNIT, &punit);
 	if (ret)
