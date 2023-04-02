a4545dcca65a4cbbd0b798194ac2b186500a9f26
Simon Glass <sjg@chromium.org>
2023-04-03 06:36:16 +1200

    riscv: Add a 64-bit image type
    At present it is not possible to know whether an image can be booted by
    a 32- or 64-bit bootloader. This means that U-Boot may attempt to boot
    the wrong image. This may cause a crash which might be hard to debug.
    
    Add a new property to make this explicit.
    
    The existing 'RISC-V' is now taken to mean 32-bit.
    
    Series-to: u-boot
    Series-cc: bin, Sean Anderson <seanga2@gmail.com>
    Series-cc: Rick Chen <rick@andestech.com>
    Series-cc: Leo <ycliang@andestech.com>
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/image.h
+++ include/image.h
@@ -138,7 +138,8 @@ enum {
 	IH_ARCH_ARC,			/* Synopsys DesignWare ARC */
 	IH_ARCH_X86_64,			/* AMD x86_64, Intel and Via */
 	IH_ARCH_XTENSA,			/* Xtensa	*/
-	IH_ARCH_RISCV,			/* RISC-V */
+	IH_ARCH_RISCV,			/* RISC-V 32-Bit */
+	IH_ARCH_RISCV64,		/* RISC-V 64 Bit */
 
 	IH_ARCH_COUNT,
 };
