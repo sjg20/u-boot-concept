qemu/u-boot

ns16550: match when to define bdf with uart code
When switching defined(CONFIG_PCI) to CONFIG_IS_ENABLED(PCI)
bdf is no longer accessible. So add preprocessor protection
to avoid access.

Signed-off-by: Troy Kisky <troykiskyboundary@gmail.com>
Reviewed-by: Simon Glass <sjg@chromium.org>

arch/x86/cpu/apollolake/uart.c
@@ -79,10 +79,12 @@ void apl_uart_init(pci_dev_t bdf, ulong base)
 
 static int apl_ns16550_probe(struct udevice *dev)
 {
+#if IS_ENABLED_NOCHECK(CONFIG_PCI) && defined(CONFIG_SPL_BUILD)
 	struct apl_ns16550_plat *plat = dev_get_plat(dev);
 
 	if (!CONFIG_IS_ENABLED(PCI))
 		apl_uart_init(plat->ns16550.bdf, plat->ns16550.base);
+#endif
 
 	return ns16550_serial_probe(dev);
 }
@@ -110,7 +112,9 @@ static int apl_ns16550_of_to_plat(struct udevice *dev)
 	ns.reg_offset = 0;
 	ns.clock = dtplat->clock_frequency;
 	ns.fcr = UART_FCR_DEFVAL;
+#if IS_ENABLED_NOCHECK(CONFIG_PCI) && defined(CONFIG_SPL_BUILD)
 	ns.bdf = pci_ofplat_get_devfn(dtplat->reg[0]);
+#endif
 	memcpy(plat, &ns, sizeof(ns));
 #else
 	int ret;

include/ns16550.h
@@ -74,7 +74,7 @@ struct ns16550_plat {
 	int clock;
 	u32 fcr;
 	int flags;
-#if defined(CONFIG_PCI) && defined(CONFIG_SPL)
+#if IS_ENABLED_NOCHECK(CONFIG_PCI) && defined(CONFIG_SPL_BUILD)
 	int bdf;
 #endif
 };

sjg20/u-boot-concept

arch/x86/cpu/apollolake/uart.c
@@ -79,10 +79,12 @@ void apl_uart_init(pci_dev_t bdf, ulong base)
 
 static int apl_ns16550_probe(struct udevice *dev)
 {
+#if IS_ENABLED(CONFIG_PCI) && defined(CONFIG_SPL_BUILD)
 	struct apl_ns16550_plat *plat = dev_get_plat(dev);
 
 	if (!CONFIG_IS_ENABLED(PCI))
 		apl_uart_init(plat->ns16550.bdf, plat->ns16550.base);
+#endif
 
 	return ns16550_serial_probe(dev);
 }
@@ -110,7 +112,9 @@ static int apl_ns16550_of_to_plat(struct udevice *dev)
 	ns.reg_offset = 0;
 	ns.clock = dtplat->clock_frequency;
 	ns.fcr = UART_FCR_DEFVAL;
+#if IS_ENABLED(CONFIG_PCI) && defined(CONFIG_SPL_BUILD)
 	ns.bdf = pci_ofplat_get_devfn(dtplat->reg[0]);
+#endif
 	memcpy(plat, &ns, sizeof(ns));
 #else
 	int ret;

include/ns16550.h
@@ -74,7 +74,7 @@ struct ns16550_plat {
 	int clock;
 	u32 fcr;
 	int flags;
-#if defined(CONFIG_PCI) && defined(CONFIG_SPL)
+#if IS_ENABLED(CONFIG_PCI) && defined(CONFIG_SPL_BUILD)
 	int bdf;
 #endif
 };