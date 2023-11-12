fca2b6d216e68ae7b6113b40880af3c361091537
Simon Glass <sjg@chromium.org>
2023-11-12 15:11:16 -0700

    dm: core: Introduce work-arounds
    We go around in circles trying to have a devicetree which suits U-Boot
    as well as Linux. Perhaps we should accept that this is never going to
    work properly, due to the imbalance of power between the two projects.
    
    However we have accumulated quite a lot of work-arounds for this
    problem, some of which even your faithful driver model maintain is
    barely aware of. This seems to be an unfortunate growth area in U-Boot.
    
    Introduce the concept of a driver model work-around (called a 'bodge')
    to encapsulate these areas. The word is unique in U-Boot and is not
    entirely negative in its connotations.
    
    Over time we can keep them in this one place so it is easier to drop
    them later, if this becomes possible.
    
    For now the function is empty. Future patches will expand it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ drivers/core/bodge.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Written by Simon Glass <sjg@chromium.org>
+ */
+
+#include <dm.h>
+#include <dm/root.h>
+
+int dm_bodge(bool pre_reloc_only)
+{
+	/* nothing yet */
+	return 0;
+}
