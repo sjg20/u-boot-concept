32c1902410d95af7b7ea0820025806413b2858e2
Simon Glass <sjg@chromium.org>
2023-11-12 16:39:33 -0700

    dm: core: Drop DM_FLAG_PROBE_AFTER_BIND
    This flag is no-longer used so drop it and the code that uses it. We
    can use the bodge system instead.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/core/bodge.c
+++ drivers/core/bodge.c
@@ -9,42 +9,6 @@
 #include <dm/root.h>
 #include <dm/device-internal.h>
 
-/**
- * dm_probe_devices() - Probe devices which want to be probed automatically
- *
- * Some devices want to be probed as soon as they are bound. Handle this by
- * checking the flag and probing as necessary.
- *
- * NOTE: there is almost no error checking on this process, so if a device failsss
- * to probe for any reason, it will be silently ignored
- *
- * @pre_reloc_only: If true, bind only nodes with special devicetree properties,
- * or drivers with the DM_FLAG_PRE_RELOC flag. If false bind all drivers.
- *
- * Return: 0 if OK, -ve it any default failed
- */
-static int dm_probe_devices(struct udevice *dev, bool pre_reloc_only)
-{
-	ofnode node = dev_ofnode(dev);
-	struct udevice *child;
-	int ret;
-
-	if (!pre_reloc_only ||
-	    (ofnode_valid(node) && ofnode_pre_reloc(node)) ||
-	    (dev->driver->flags & DM_FLAG_PRE_RELOC)) {
-		if (dev_get_flags(dev) & DM_FLAG_PROBE_AFTER_BIND) {
-			ret = device_probe(dev);
-			if (ret)
-				return ret;
-		}
-	}
-
-	list_for_each_entry(child, &dev->child_head, sibling_node)
-		dm_probe_devices(child, pre_reloc_only);
-
-	return 0;
-}
-
 /*
  * dm_bodge_probe() - handle auto-probing of particular devices
  *
@@ -62,10 +26,6 @@ static int dm_bodge_probe(bool pre_reloc_only)
 	struct udevice *dev;
 	struct uclass *uc;
 	int ret;
-
-	ret = dm_probe_devices(gd->dm_root, pre_reloc_only);
-	if (ret)
-		return ret;
 
 	/* probe all the GPIO hogs that were bound  */
 	if (CONFIG_IS_ENABLED(GPIO_HOG)) {
