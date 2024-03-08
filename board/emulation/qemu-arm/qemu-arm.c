42d79d62f2ba10ab004245c05bc3d3f7384a8222
Alexander Graf <agraf@csgraf.de>
2024-03-08 10:28:01 +0800

    arm: qemu: Enable ramfb by default
    Now that we have everything in place to support ramfb, let's wire it up
    by default in the ARM QEMU targets. That way, you can easily use a
    graphical console by just passing -device ramfb to the QEMU command line.
    
    Signed-off-by: Alexander Graf <agraf@csgraf.de>
    [Alper: Rebase on bochs changes, add pre-reloc init, error handling]
    Co-developed-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>
    [bmeng: Rebase on u-boot/master]
    Signed-off-by: Bin Meng <bmeng@tinylab.org>

--- board/emulation/qemu-arm/qemu-arm.c
+++ board/emulation/qemu-arm/qemu-arm.c
@@ -11,6 +11,7 @@
 #include <fdtdec.h>
 #include <init.h>
 #include <log.h>
+#include <qfw.h>
 #include <usb.h>
 #include <virtio_types.h>
 #include <virtio.h>
@@ -101,6 +102,46 @@ static struct mm_region qemu_arm64_mem_map[] = {
 
 struct mm_region *mem_map = qemu_arm64_mem_map;
 #endif
+
+int board_early_init_f(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	/*
+	 * Make sure we enumerate the QEMU Firmware device to bind ramfb
+	 * so video_reserve() can reserve memory for it.
+	 */
+	if (IS_ENABLED(CONFIG_QFW)) {
+		ret = qfw_get_dev(&dev);
+		if (ret) {
+			log_err("Failed to get QEMU FW device: %d\n", ret);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+int board_early_init_r(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	/*
+	 * Make sure we enumerate the QEMU Firmware device to find ramfb
+	 * before console_init.
+	 */
+	if (IS_ENABLED(CONFIG_QFW)) {
+		ret = qfw_get_dev(&dev);
+		if (ret) {
+			log_err("Failed to get QEMU FW device: %d\n", ret);
+			return ret;
+		}
+	}
+
+	return 0;
+}
 
 int board_init(void)
 {
