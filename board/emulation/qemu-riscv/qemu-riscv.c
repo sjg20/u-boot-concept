279c306796ccf6c90e5e1b2db42bc7632a429e12
Alexander Graf <agraf@csgraf.de>
2024-03-08 11:04:55 +0800

    riscv: qemu: Enable ramfb by default
    Now that we have everything in place to support ramfb, let's wire it up
    by default in the RISC-V QEMU targets. That way, you can easily use a
    graphical console by just passing -device ramfb to the QEMU command line.
    
    Signed-off-by: Alexander Graf <agraf@csgraf.de>
    [Alper: Rebase on bochs changes, add pre-reloc init and error handling]
    Co-developed-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>
    [bmeng: Rebase on u-boot/master]
    Signed-off-by: Bin Meng <bmeng@tinylab.org>

--- board/emulation/qemu-riscv/qemu-riscv.c
+++ board/emulation/qemu-riscv/qemu-riscv.c
@@ -10,6 +10,7 @@
 #include <fdtdec.h>
 #include <image.h>
 #include <log.h>
+#include <qfw.h>
 #include <spl.h>
 #include <init.h>
 #include <usb.h>
@@ -28,6 +29,46 @@ int is_flash_available(void)
 	return 0;
 }
 #endif
+
+int board_early_init_f(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	/*
+	 * Make sure we enumerate the QEMU Firmware device to bind ramfb
+	 * so video_reserve() can reserve memory for it.
+	 */
+	if (IS_ENABLED(CONFIG_QFW)) {
+		ret = qfw_get_dev(&dev);
+		if (ret) {
+			log_err("Failed to get QEMU FW device: %d\n", ret);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+int board_early_init_r(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	/*
+	 * Make sure we enumerate the QEMU Firmware device to find ramfb
+	 * before console_init.
+	 */
+	if (IS_ENABLED(CONFIG_QFW)) {
+		ret = qfw_get_dev(&dev);
+		if (ret) {
+			log_err("Failed to get QEMU FW device: %d\n", ret);
+			return ret;
+		}
+	}
+
+	return 0;
+}
 
 int board_init(void)
 {
