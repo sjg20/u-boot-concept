ea5bb7312532ca4196c9d74fc55c68fc845bc0d4
Simon Glass <sjg@chromium.org>
2024-05-23 19:09:16 -0600

    WIP: test: Release board

--- test/py/u_boot_console_exec_attach.py
+++ test/py/u_boot_console_exec_attach.py
@@ -70,3 +70,12 @@ class ConsoleExecAttach(ConsoleBase):
             raise
 
         return s
+
+    def close(self):
+        self.log.action('Releasing board')
+        args = [self.config.board_type, self.config.board_identity]
+        cmd = ['u-boot-test-release'] + args
+        runner = self.log.get_runner(cmd[0], sys.stdout)
+        runner.run(cmd)
+        runner.close()
+        super().close()
