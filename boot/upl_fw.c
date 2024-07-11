26e549d5ee2903d5af78d0276b58d1b6b47b44df
Simon Glass <sjg@chromium.org>
2024-07-11 10:50:29 +0100

    wip

---
+++ boot/upl_fw.c
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Universal Payload (UPL) loading firmware phases
+ *
+ * Copyright 2024 Google LLC
+ * Written by Simon Glass <sjg@chromium.org>
+ */
+
+#define LOG_CATEGORY LOGC_BOOT
+
+#include <bootdev.h>
+#include <spl.h>
+
+static int upl_load_from_image(struct spl_image_info *spl_image,
+			       struct spl_boot_device *bootdev)
+{
+	printf("upl\n");
+
+	return 0;
+}
+SPL_LOAD_IMAGE_METHOD("upl", 5, BOOT_DEVICE_UPL, upl_load_from_image);
