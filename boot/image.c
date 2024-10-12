7a85919c2dc88d3261781df1487cda7e6b888bc7
Simon Glass <sjg@chromium.org>
2024-10-12 15:15:43 -0600

    image: Add a new type for extlinux
    Add an image type for the extlinux.cfg file, which U-Boot supports
    reading.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- boot/image.c
+++ boot/image.c
@@ -183,6 +183,7 @@ static const table_entry_t uimage_type[] = {
 	{	IH_TYPE_FDT_LEGACY, "fdt_legacy", "legacy Image with Flat Device Tree ", },
 	{	IH_TYPE_RENESAS_SPKG, "spkgimage", "Renesas SPKG Image" },
 	{	IH_TYPE_STARFIVE_SPL, "sfspl", "StarFive SPL Image" },
+	{	IH_TYPE_EXTLINUX_CFG, "extlinux_cfg", "Extlinux configuration" },
 	{	-1,		    "",		  "",			},
 };
 
