ffe777277bae4f37d4de66ca0b81d09709b9b346
Simon Glass <sjg@chromium.org>
2024-10-16 14:52:52 -0600

    Revert "lib: uuid: supporting building as part of host tools"
    This reverts commit 22c48a92cdcead3c7bfb1449528d275bf8d3d30a.
    
    Series-to: u-boot
    Series-cc: Caleb Connolly <caleb.connolly@linaro.org>
    Cover-letter:
    This is a very strange series and I am sending it for comment, since I
    must be missing something.
    
    I see a problem on chromebook_coral where mmc stops working. The symptom
    is that 'bootflow scan' does not find ChromeOS on mmc0.
    
    I have bisected to this commit:
    
       22c48a92cdc lib: uuid: supporting building as part of host tools
    
    I did this twice, because I didn't beleive it the first time. I also
    
    END

--- lib/uuid.c
+++ lib/uuid.c
@@ -7,35 +7,22 @@
  *   Abdellatif El Khlifi <abdellatif.elkhlifi@arm.com>
  */
 
-#ifndef USE_HOSTCC
+#define LOG_CATEGOT LOGC_CORE
+
 #include <command.h>
 #include <efi_api.h>
 #include <env.h>
 #include <rand.h>
 #include <time.h>
+#include <uuid.h>
+#include <linux/ctype.h>
+#include <errno.h>
 #include <asm/io.h>
 #include <part_efi.h>
 #include <malloc.h>
 #include <dm/uclass.h>
 #include <rng.h>
-#include <linux/ctype.h>
-#include <hexdump.h>
-#else
-#include <stdarg.h>
-#include <stdint.h>
-#include <eficapsule.h>
-#include <ctype.h>
-#endif
-#include <linux/types.h>
-#include <errno.h>
-#include <linux/kconfig.h>
-#include <uuid.h>
 #include <u-boot/sha1.h>
-
-#ifdef USE_HOSTCC
-/* polyfill hextoul to avoid pulling in strto.c */
-#define hextoul(cp, endp) strtoul(cp, endp, 16)
-#endif
 
 int uuid_str_valid(const char *uuid)
 {
@@ -65,7 +52,6 @@ static const struct {
 	const char *string;
 	efi_guid_t guid;
 } list_guid[] = {
-#ifndef USE_HOSTCC
 #ifdef CONFIG_PARTITION_TYPE_GUID
 	{"system",	PARTITION_SYSTEM_GUID},
 	{"mbr",		LEGACY_MBR_PARTITION_GUID},
@@ -246,7 +232,6 @@ static const struct {
 	{ "EFI_MEM_STATUS_CODE_REC", EFI_MEM_STATUS_CODE_REC },
 	{ "EFI_GUID_EFI_ACPI1", EFI_GUID_EFI_ACPI1 },
 #endif
-#endif /* !USE_HOSTCC */
 };
 
 int uuid_guid_get_bin(const char *guid_str, unsigned char *guid_bin)
@@ -282,6 +267,7 @@ int uuid_str_to_bin(const char *uuid_str, unsigned char *uuid_bin,
 	uint64_t tmp64;
 
 	if (!uuid_str_valid(uuid_str)) {
+		log_debug("not valid\n");
 #ifdef CONFIG_PARTITION_TYPE_GUID
 		if (!uuid_guid_get_bin(uuid_str, uuid_bin))
 			return 0;
@@ -312,7 +298,7 @@ int uuid_str_to_bin(const char *uuid_str, unsigned char *uuid_bin,
 	tmp16 = cpu_to_be16(hextoul(uuid_str + 19, NULL));
 	memcpy(uuid_bin + 8, &tmp16, 2);
 
-	tmp64 = cpu_to_be64(hextoul(uuid_str + 24, NULL));
+	tmp64 = cpu_to_be64(simple_strtoull(uuid_str + 24, NULL, 16));
 	memcpy(uuid_bin + 10, (char *)&tmp64 + 2, 6);
 
 	return 0;
@@ -320,9 +306,9 @@ int uuid_str_to_bin(const char *uuid_str, unsigned char *uuid_bin,
 
 int uuid_str_to_le_bin(const char *uuid_str, unsigned char *uuid_bin)
 {
-	uint16_t tmp16;
-	uint32_t tmp32;
-	uint64_t tmp64;
+	u16 tmp16;
+	u32 tmp32;
+	u64 tmp64;
 
 	if (!uuid_str_valid(uuid_str) || !uuid_bin)
 		return -EINVAL;
@@ -339,7 +325,7 @@ int uuid_str_to_le_bin(const char *uuid_str, unsigned char *uuid_bin)
 	tmp16 = cpu_to_le16(hextoul(uuid_str + 19, NULL));
 	memcpy(uuid_bin + 8, &tmp16, 2);
 
-	tmp64 = cpu_to_le64(hextoul(uuid_str + 24, NULL));
+	tmp64 = cpu_to_le64(simple_strtoull(uuid_str + 24, NULL, 16));
 	memcpy(uuid_bin + 10, &tmp64, 6);
 
 	return 0;
@@ -348,11 +334,11 @@ int uuid_str_to_le_bin(const char *uuid_str, unsigned char *uuid_bin)
 void uuid_bin_to_str(const unsigned char *uuid_bin, char *uuid_str,
 		     int str_format)
 {
-	const uint8_t uuid_char_order[UUID_BIN_LEN] = {0, 1, 2, 3, 4, 5, 6, 7, 8,
+	const u8 uuid_char_order[UUID_BIN_LEN] = {0, 1, 2, 3, 4, 5, 6, 7, 8,
 						  9, 10, 11, 12, 13, 14, 15};
-	const uint8_t guid_char_order[UUID_BIN_LEN] = {3, 2, 1, 0, 5, 4, 7, 6, 8,
+	const u8 guid_char_order[UUID_BIN_LEN] = {3, 2, 1, 0, 5, 4, 7, 6, 8,
 						  9, 10, 11, 12, 13, 14, 15};
-	const uint8_t *char_order;
+	const u8 *char_order;
 	const char *format;
 	int i;
 
@@ -434,7 +420,6 @@ void gen_v5_guid(const struct uuid *namespace, struct efi_guid *guid, ...)
 	*tmp16 = cpu_to_le16(be16_to_cpu(*tmp16));
 }
 
-#ifndef USE_HOSTCC
 #if defined(CONFIG_RANDOM_UUID) || defined(CONFIG_CMD_UUID)
 void gen_rand_uuid(unsigned char *uuid_bin)
 {
@@ -518,4 +503,3 @@ U_BOOT_CMD(guid, CONFIG_SYS_MAXARGS, 1, do_uuid,
 );
 #endif /* CONFIG_CMD_UUID */
 #endif /* CONFIG_RANDOM_UUID || CONFIG_CMD_UUID */
-#endif /* !USE_HOSTCC */
