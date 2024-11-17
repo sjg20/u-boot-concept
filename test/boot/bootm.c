c4a6695c0a7f07dcf00ef627e269dea0cca67302
Simon Glass <sjg@chromium.org>
2024-11-17 16:59:04 -0700

    test: Update bootm test to restore silent_console
    Unset this environment variable at the end of bootm_test_silent() et al
    to avoid affecting subsequent tests.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/boot/bootm.c
+++ test/boot/bootm.c
@@ -121,6 +121,9 @@ static int bootm_test_silent(struct unit_test_state *uts)
 	ut_assertok(bootm_process_cmdline(buf, BUF_SIZE, BOOTM_CL_SILENT));
 	ut_asserteq_str("console=ttynull something", buf);
 
+	/* restore settings */
+	env_set("silent_linux", NULL);
+
 	return 0;
 }
 BOOTM_TEST(bootm_test_silent, 0);
@@ -226,6 +229,8 @@ static int bootm_test_subst_var(struct unit_test_state *uts)
 	ut_assertok(bootm_process_cmdline_env(BOOTM_CL_SILENT));
 	ut_asserteq_str("some${var}thing console=ttynull", env_get("bootargs"));
 
+	env_set("silent_linux", NULL);
+
 	return 0;
 }
 BOOTM_TEST(bootm_test_subst_var, 0);
@@ -242,6 +247,8 @@ static int bootm_test_subst_both(struct unit_test_state *uts)
 	ut_assertok(env_set("var", "1234567890"));
 	ut_assertok(bootm_process_cmdline_env(BOOTM_CL_ALL));
 	ut_asserteq_str("some1234567890thing console=ttynull", env_get("bootargs"));
+
+	env_set("silent_linux", NULL);
 
 	return 0;
 }
