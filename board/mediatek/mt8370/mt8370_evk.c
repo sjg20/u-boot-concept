6dc1d792eba15838933af6df6b08bf0331021e88
Tommy Chen <tommyyl.chen@mediatek.com>
2024-11-20 10:29:58 +0800

    GENIO: mt8370: bootlogo: add splash_locations for qspi
    Add genio_510_evk_spi_splash_locations to specify the storage type,
    flags, and offset of the splash source. Also, add a
    splash_screen_prepare function to override the one defined in
    the splash.c. The splash source will use the splash locations defined.
    
    Change-Id: Ie2f8cccb3682c077db50dd85f057f622d0829034
    Signed-off-by: Tommy Chen <tommyyl.chen@mediatek.com>

--- board/mediatek/mt8370/mt8370_evk.c
+++ board/mediatek/mt8370/mt8370_evk.c
@@ -12,6 +12,7 @@
 #include <asm/io.h>
 #include <linux/kernel.h>
 #include <linux/arm-smccc.h>
+#include <splash.h>
 
 #define MT8370_UPDATABLE_IMAGES	5
 
@@ -131,6 +132,23 @@ void set_dfu_alt_info(char *interface, char *devstr)
 }
 #endif
 #endif /* CONFIG_EFI_HAVE_CAPSULE_SUPPORT && CONFIG_EFI_PARTITION */
+
+#if IS_ENABLED(CONFIG_SPLASH_SCREEN) && IS_ENABLED(CONFIG_SPI_FLASH)
+static struct splash_location genio_510_evk_spi_splash_locations[] = {
+	{
+		.name = "sf",
+		.storage = SPLASH_STORAGE_SF,
+		.flags = SPLASH_STORAGE_RAW,
+		.offset = 0x1580000,
+	},
+};
+
+int splash_screen_prepare(void)
+{
+	return splash_source_load(genio_510_evk_spi_splash_locations,
+				  ARRAY_SIZE(genio_510_evk_spi_splash_locations));
+}
+#endif
 
 int board_init(void)
 {
