u-boot/u-boot

tpm: Rename tpm2_allow_extend()
When that function was introduced we were only using it to check if
extending a PCR was allowed, so the name made sense. A few patches ago
we used that function to reason about the EventLog creation and general
usage of PCRs , so let's rename it to something more generic that makes
more sense in all contexts.

Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

lib/tpm-v2.c
@@ -197,7 +197,7 @@ u32 tpm2_pcr_extend(struct udevice *dev, u32 index, u32 algorithm,
 	if (!digest)
 		return -EINVAL;
 
-	if (!tpm2_allow_extend(dev)) {
+	if (!tpm2_check_active_pcrs(dev)) {
 		log_err("Cannot extend PCRs if all the TPM enabled algorithms are not supported\n");
 		return -EINVAL;
 	}
@@ -896,7 +896,7 @@ u16 tpm2_algorithm_to_len(enum tpm2_algorithms algo)
 	return 0;
 }
 
-bool tpm2_allow_extend(struct udevice *dev)
+bool tpm2_check_active_pcrs(struct udevice *dev)
 {
 	struct tpml_pcr_selection pcrs;
 	size_t i;

lib/tpm_tcg2.c
@@ -579,7 +579,7 @@ int tcg2_log_prepare_buffer(struct udevice *dev, struct tcg2_event_log *elog,
 	 * since we are about to create an EventLog and we won't
 	 * measure anything if the PCR banks don't match
 	 */
-	if (!tpm2_allow_extend(dev)) {
+	if (!tpm2_check_active_pcrs(dev)) {
                 log_err("Cannot create EventLog\n");
                 log_err("Mismatch between U-Boot and TPM hash algos\n");
                 log_err("TPM:\n");

sjg20/u-boot-concept

lib/tpm-v2.c
@@ -197,7 +197,7 @@ u32 tpm2_pcr_extend(struct udevice *dev, u32 index, u32 algorithm,
 	if (!digest)
 		return -EINVAL;
 
-	if (!tpm2_allow_extend(dev)) {
+	if (!tpm2_check_active_banks(dev)) {
 		log_err("Cannot extend PCRs if all the TPM enabled algorithms are not supported\n");
 		return -EINVAL;
 	}
@@ -896,7 +896,7 @@ u16 tpm2_algorithm_to_len(enum tpm2_algorithms algo)
 	return 0;
 }
 
-bool tpm2_allow_extend(struct udevice *dev)
+bool tpm2_check_active_banks(struct udevice *dev)
 {
 	struct tpml_pcr_selection pcrs;
 	size_t i;

lib/tpm_tcg2.c
@@ -579,7 +579,7 @@ int tcg2_log_prepare_buffer(struct udevice *dev, struct tcg2_event_log *elog,
 	 * since we are about to create an EventLog and we won't
 	 * measure anything if the PCR banks don't match
 	 */
-	if (!tpm2_allow_extend(dev)) {
+	if (!tpm2_check_active_banks(dev)) {
                 log_err("Cannot create EventLog\n");
                 log_err("Mismatch between U-Boot and TPM hash algos\n");
                 log_err("TPM:\n");