7ac6c4492f4792cb520a8af5a7d8ae0ef33b5429
Simon Glass <sjg@chromium.org>
2025-01-11 17:17:00 -0700

    test: Drop the function for running measurement tests
    Use the new suite-runner to run these tests instead.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/cmd_ut.c
+++ test/cmd_ut.c
@@ -166,7 +166,7 @@ static struct suite suites[] = {
 	SUITE(setexpr),
 #endif
 #ifdef CONFIG_MEASURED_BOOT
-	SUITE_CMD(measurement, do_ut_measurement),
+	SUITE(measurement),
 #endif
 #ifdef CONFIG_SANDBOX
 	SUITE_CMD(bloblist, do_ut_bloblist),
