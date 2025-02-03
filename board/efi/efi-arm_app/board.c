e6cc7e366aa10e53e7e3d9fce9d83b6b237504e0
Simon Glass <sjg@chromium.org>
2025-02-02 17:56:11 -0700

    efi: arm: Add an EFI app for arm64
    Introduce an EFI app for arm64 and update the documentation.
    
    Provide a value for LOAD_ADDR to avoid a link error.
    
    Series-to: u-boot
    Series-cc: heinrich
    Series-links: 442676
    Series-version: 3
    Series-changes: 2
    - Use ARCH_EFI instead of VENDOR_EFI
    - Merge the linker-script rules into Kconfig in this patch
    - Drop patch 'Select the EFI linker script for the app'
    
    Cover-letter:
    arm: Support building as an EFI app
    The original x86 EFI-app was written back in 2015, about a year before
    the EFI-loader was introduced. At that point, ARM mostly used U-Boot and
    FIT (Flat Image Tree).
    
    Today, sadly, there are a few efforts to re-invent FIT poorly, to cater
    to more primitive boot-environments than U-Boot. One justification for
    this re-invetion has been that EDK2 did not support FIT.
    
    EDK2 does now support FIT but only for the firmware stage. Even then,
    vendors tend to use their own forks, so any new features are slow to
    make it into the market.
    
    EDK2 also supports the ext4 filesystem, but many devices don't include
    that feature, so most distros make use of FAT for storing their boot
    files. Since FAT is not journalled, it is not particularly reliable in
    the face of reset, power failure, etc.
    
    As an interim measure, we can use U-Boot as an EFI app running on top of
    whatever EDK2 fork is in use. This allows use of modern features such as
    FIT and filesystems other than FAT. So far this only supports x86, but
    recent ARM laptops are making use of EFI too.
    
    So this series adds an EFI app for 64-bit ARM.
    
    Note: Since U-Boot does not support EFI's GOP 'blt-only' mode, the
    vidconsole does not operate, but it can still be used, since EDK2
    handles drawing instead of U-Boot. As a result, the U-Boot logo is not
    shown.
    END
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ board/efi/efi-arm_app/board.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2015 Google, Inc
+ */
+
+#include <init.h>
+
+struct mm_region *mem_map;
+
+int print_cpuinfo(void)
+{
+	return 0;
+}
+
+int board_init(void)
+{
+	return 0;
+}
