d6f5aaa644585e53e97f63819b0978a08f7e41a4
Simon Glass <sjg@chromium.org>
2025-03-14 12:14:21 +0000

    expo: Split setting up the menu from adding items
    Some callers may wish to add items later as they are discovered. Split
    the setup code into its own function, to permit this.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- boot/bootflow_menu.c
+++ boot/bootflow_menu.c
@@ -252,7 +252,7 @@ int bootflow_menu_apply_theme(struct expo *exp, ofnode node)
 	return 0;
 }
 
-int bootflow_menu_start(struct bootstd_priv *std, bool text_mode,
+int bootflow_menu_setup(struct bootstd_priv *std, bool text_mode,
 			struct expo **expp)
 {
 	struct udevice *dev;
@@ -260,7 +260,6 @@ int bootflow_menu_start(struct bootstd_priv *std, bool text_mode,
 	int ret;
 
 	LOGR("bmn", bootflow_menu_new(&exp));
-	LOGR("bma", bootflow_menu_add_all(exp));
 
 	if (ofnode_valid(std->theme)) {
 		ret = bootflow_menu_apply_theme(exp, std->theme);
@@ -282,6 +281,27 @@ int bootflow_menu_start(struct bootstd_priv *std, bool text_mode,
 
 	if (text_mode)
 		expo_set_text_mode(exp, text_mode);
+
+	*expp = exp;
+
+	return 0;
+}
+
+int bootflow_menu_start(struct bootstd_priv *std, bool text_mode,
+			struct expo **expp)
+{
+	struct expo *exp;
+	int ret;
+
+	LOGR("bmn", bootflow_menu_setup(std, text_mode, &exp));
+
+	LOGR("bma", bootflow_menu_add_all(exp));
+
+	if (ofnode_valid(std->theme)) {
+		ret = expo_apply_theme(exp, std->theme);
+		if (ret)
+			return log_msg_ret("thm", ret);
+	}
 
 	LOGR("bmd", expo_calc_dims(exp));
 
