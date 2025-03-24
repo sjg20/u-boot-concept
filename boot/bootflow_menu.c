eb2708b8de15a3d999ab10d2e3ab966b3de97191
Simon Glass <sjg@chromium.org>
2025-03-24 06:41:11 +0000

    expo: Allow the nominal display-size to be selected
    At present all coordinates are absolute and must fit within the display.
    This makes it hard to create an expo which can work on any size of
    display, a key goal of expo.
    
    Add the concept of a normal size, to which all coordinates conform. Once
    the real display-size is known, expo can in principle (i.e. with later
    work) scale the coordinates and objects according.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- boot/bootflow_menu.c
+++ boot/bootflow_menu.c
@@ -49,6 +49,7 @@ int bootflow_menu_new(struct expo **expp)
 	ret = expo_new("bootflows", priv, &exp);
 	if (ret)
 		return log_msg_ret("exp", ret);
+	expo_req_size(exp, 1366, 768);
 
 	ret = scene_new(exp, "main", MAIN, &scn);
 	if (ret < 0)
