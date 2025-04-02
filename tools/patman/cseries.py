f63497db4d0a247e13d675d6241183d52d5e94cd
Simon Glass <sjg@chromium.org>
2025-04-02 14:39:23 +1300

    ok

--- tools/patman/cseries.py
+++ tools/patman/cseries.py
@@ -645,8 +645,8 @@ class Cseries:
         pcd = self.get_pcommit_dict(old_pwid)
 
         res = self.cur.execute(
-            'INSERT INTO ser_ver (version, series_id) VALUES'
-            f"('{vers}', {ser.idnum})")
+            'INSERT INTO ser_ver (series_id, version) VALUES'
+            f"({ser.idnum}, '{vers}')")
         pwid = self.cur.lastrowid
 
         for pcm in pcd.values():
