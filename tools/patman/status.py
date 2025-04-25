7722295ff960ba3124db84e2b675f8f4bfe06b65
Simon Glass <sjg@chromium.org>
2025-04-25 09:39:50 -0600

    wip

--- tools/patman/status.py
+++ tools/patman/status.py
@@ -96,7 +96,7 @@ def create_branch(series, new_rtag_list, branch, dest_branch, overwrite,
     return num_added
 
 
-async def _check_status(client, cover, patches, series, series_id, branch, dest_branch, force,
+async def _check_status(client, cover, patches, series, link, branch, dest_branch, force,
                         show_comments, show_cover_comments, pwork,
                         test_repo=None):
     """Check the status of a series on Patchwork
@@ -107,7 +107,7 @@ async def _check_status(client, cover, patches, series, series_id, branch, dest_
     Args:
         client (aiohttp.ClientSession): Session to use
         series (Series): Series object for the existing branch
-        series_id (str): Patch series ID number
+        link (str): Patch series ID number
         branch (str): Existing branch to update, or None
         dest_branch (str): Name of new branch to create, or None
         force (bool): True to force overwriting dest_branch if it exists
@@ -119,7 +119,7 @@ async def _check_status(client, cover, patches, series, series_id, branch, dest_
     """
     with terminal.pager():
         num_to_add, new_rtag_list, _, _ = await pwork._check_status(
-            client, cover, patches, series, series_id, branch, show_comments,
+            client, cover, patches, series, link, branch, show_comments,
             show_cover_comments)
 
         if not dest_branch and num_to_add:
@@ -138,51 +138,51 @@ async def _check_status(client, cover, patches, series, series_id, branch, dest_
                 f"from patchwork into new branch '{dest_branch}'")
 
 
-async def check_status(cover, patches, series, series_id, branch, dest_branch, force,
+async def check_status(cover, patches, series, link, branch, dest_branch, force,
                        show_comments, show_cover_comments, patchwork,
                        test_repo=None):
     async with aiohttp.ClientSession() as client:
         await _check_status(
-            client, cover, patches, series, series_id, branch, dest_branch,  force,
+            client, cover, patches, series, link, branch, dest_branch,  force,
             show_comments, show_cover_comments, patchwork, test_repo=test_repo)
 
 
-def check_patchwork_status(cover, patches, series, series_id, branch, dest_branch, force,
+def check_patchwork_status(cover, patches, series, link, branch, dest_branch, force,
                            show_comments, show_cover_comments, patchwork,
                            test_repo=None, single_thread=False):
     if single_thread:
-        asyncio.run(check_status(cover, patches, series, series_id, branch, dest_branch, force,
+        asyncio.run(check_status(cover, patches, series, link, branch, dest_branch, force,
                                  show_comments, show_cover_comments, patchwork,
                                  test_repo=test_repo))
     else:
         loop = asyncio.get_event_loop()
         loop.run_until_complete(check_status(
-                cover, patches, series, series_id, branch, dest_branch,  force, show_comments,
+                cover, patches, series, link, branch, dest_branch,  force, show_comments,
                 show_cover_comments, patchwork, test_repo=test_repo))
 
 
 #######################
-async def check_and_report_status(series, series_id, branch, dest_branch, force,
+async def check_and_report_status(series, link, branch, dest_branch, force,
                        show_comments, show_cover_comments, patchwork,
                        test_repo=None):
     async with aiohttp.ClientSession() as client:
         cover, patches = await patchwork._collect_patches(
-            client, len(series.commits), series_id, True, show_cover_comments)
+            client, len(series.commits), link, True, show_cover_comments)
         await _check_status(
-            client, cover, patches, series, series_id, branch, dest_branch,  force,
+            client, cover, patches, series, link, branch, dest_branch,  force,
             show_comments, show_cover_comments, patchwork, test_repo=test_repo)
 
 
-def check_and_report_patchwork_status(series, series_id, branch, dest_branch, force,
+def check_and_report_patchwork_status(series, link, branch, dest_branch, force,
                            show_comments, show_cover_comments, patchwork,
                            test_repo=None, single_thread=False):
     if single_thread:
-        asyncio.run(check_and_report_status(series, series_id, branch, dest_branch, force,
+        asyncio.run(check_and_report_status(series, link, branch, dest_branch, force,
                                  show_comments, show_cover_comments, patchwork,
                                  test_repo=test_repo))
     else:
         loop = asyncio.get_event_loop()
         loop.run_until_complete(check_and_report_status(
-                series, series_id, branch, dest_branch,  force, show_comments,
+                series, link, branch, dest_branch,  force, show_comments,
                 show_cover_comments, patchwork, test_repo=test_repo))
 
