31f984589b72db809155de045864664bd6bb8668
Simon Glass <sjg@chromium.org>
2025-05-02 08:46:55 -0600

    expo: Update bootflow_menu_poll() to return a sequence ID
    Rather than returning a bootflow, return the index of the bootflow. This
    will allow callers to do their own translation to bootflows or some
    other data structure.
    
    Also return a special code when the user tries to move the pointer, so
    that the caller can cancel the boot-menu timeout, if this is in use.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- cmd/bootflow.c
+++ cmd/bootflow.c
@@ -108,18 +108,21 @@ __maybe_unused static int bootflow_handle_menu(struct bootstd_priv *std,
 {
 	struct expo *exp;
 	struct bootflow *bflow;
-	int ret;
+	int ret, seq;
 
 	ret = bootflow_menu_start(std, text_mode, &exp);
 	if (ret)
 		return log_msg_ret("bhs", ret);
 
+	ret = -ERESTART;
 	do {
-		ret = expo_render(exp);
-		if (ret)
-			return log_msg_ret("bhr", ret);
-		ret = bootflow_menu_poll(exp, &bflow);
-	} while (ret == -EAGAIN);
+		if (ret == -ERESTART) {
+			ret = expo_render(exp);
+			if (ret)
+				return log_msg_ret("bhr", ret);
+		}
+		ret = bootflow_menu_poll(exp, &seq);
+	} while (ret == -EAGAIN || ret == -ERESTART);
 
 	if (ret == -EPIPE) {
 		printf("Nothing chosen\n");
@@ -127,6 +130,7 @@ __maybe_unused static int bootflow_handle_menu(struct bootstd_priv *std,
 	} else if (ret) {
 		printf("Menu failed (err=%d)\n", ret);
 	} else {
+		bflow = alist_getw(&std->bootflows, seq, struct bootflow);
 		printf("Selected: %s\n", bflow->os_name ? bflow->os_name :
 		       bflow->name);
 		std->cur_bootflow = bflow;
