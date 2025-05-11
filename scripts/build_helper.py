3a6ba29bd224e42dd07b342afe433696de683e0e
Simon Glass <sjg@chromium.org>
2025-05-11 16:18:19 +0200

    scripts: Create a common settings file for QEMU scripts
    Move the settings into a common file so they can be used by all tools.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- scripts/build_helper.py
+++ scripts/build_helper.py
@@ -4,6 +4,7 @@
 
 """
 
+import configparser
 import contextlib
 import os
 import shutil
@@ -27,6 +28,70 @@ import fs_helper
 class Helper:
     def __init__(self):
         self.settings = None
+
+    def read_settings(self):
+        """Get settings from the settings file"""
+        self.settings = configparser.ConfigParser()
+        fname = f'{os.getenv("HOME")}/.u_boot_qemu'
+        if not os.path.exists(fname):
+            print('No config file found: {fname}\nCreating one...\n')
+            tools.write_file(fname, '''# U-Boot QEMU-scripts config
+
+[DEFAULT]
+# Image-output filename
+image_file = try.img
+
+# Image directory (for prebuilt UEFI-images)
+image_dir = ~/dev/uefi
+
+# Set ubdir to the build directory where you build U-Boot out-of-tree
+# We avoid in-tree build because it gets confusing trying different builds
+build_dir = /tmp/b
+
+# Build the kernel with: make O=/tmp/kernel
+bzimage = /tmp/kernel/arch/x86/boot/bzImage
+
+# Directory where OVMF-pure-efi.i386.fd etc. are kept
+efi_dir = ~/dev/efi
+
+# Directory where SCT image (sct.img) is kept
+sct_dir = ~/dev/efi/sct
+
+# Directory where the SCT image is temporarily mounted for modification
+sct_mnt = /mnt/sct
+
+# Directory containing Tianocore builds (e.g. OVMF-pure-efi.x64.fd)
+tianocore_dir = ~/dev/efi/tianocore
+
+''', binary=False)
+        self.settings.read(fname)
+
+    def get_setting(self, name, fallback=None):
+        """Get a setting by name
+
+        Args:
+            name (str): Name of setting to retrieve
+            fallback (str or None): Value to return if the setting is missing
+        """
+        raw = self.settings.get('DEFAULT', name, fallback=fallback)
+        return os.path.expandvars(os.path.expanduser(raw))
+
+    def stage(self, name):
+        """Context manager to count requests across a range of patchwork calls
+
+        Args:
+            name (str): Stage name
+
+        Return:
+            _Stage: contect object
+
+        Usage:
+            with self.stage('name'):
+                ...do things
+
+            Note that the output only appears if the -N flag is used
+        """
+        return self._Stage(name)
 
     @contextlib.contextmanager
     def make_disk(self, fname, size_mb=20, fs_type='ext4', use_part=False):
