44f1b515f9ee92e7a42e5ec32043a126b7813091
Simon Glass <sjg@chromium.org>
2025-06-09 16:41:04 -0600

    efi_loader: Support loading a ramdisk with bootefi
    It is sometimes useful to be able to boot via EFI using a ramdisk. Add
    support for this, using the presence of a separating colon to indicate
    whether the second parameter is ramdisk or FDT.
    
    Fix a 'specifiy' typo while here.
    
    Series-to: heinrich
    Series-version: 2
    Series-changes: 1
    - Drop mention of the '-' argument-value as it is not needed
    
    Series-changes: 2
    - Use : to determine whether the ramdisk argument is present
    
    Fixes: #8
    
    Co-developed-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- cmd/bootefi.c
+++ cmd/bootefi.c
@@ -133,22 +133,37 @@ static int do_bootefi(struct cmd_tbl *cmdtp, int flag, int argc,
 {
 	efi_status_t ret;
 	char *p;
-	void *fdt, *image_buf;
-	unsigned long addr, size;
+	void *fdt, *ramdisk = NULL, *image_buf;
+	unsigned long addr, size, rd_len = 0, fdt_addr = 0;
 	void *image_addr;
 	size_t image_size;
+	uint fdt_arg = 2;
+	const char *arg;
 
 	if (argc < 2)
 		return CMD_RET_USAGE;
 
 	if (argc > 2) {
-		uintptr_t fdt_addr;
+		ulong addr = hextoul(argv[2], NULL);
+		char *end = strchr(argv[2], ':');
 
-		fdt_addr = hextoul(argv[2], NULL);
-		fdt = map_sysmem(fdt_addr, 0);
-	} else {
-		fdt = EFI_FDT_USE_INTERNAL;
+		/* is this a ramdisk parameter? */
+		if (end) {
+			rd_len = hextoul(++end, NULL);
+			ramdisk = map_sysmem(addr, rd_len);
+			fdt_arg = 3;
+		} else if (argc > 3) {
+			return CMD_RET_USAGE;
+		}
 	}
+	arg = cmd_arg_get(argc, argv, fdt_arg);
+	if (arg)
+		fdt_addr = hextoul(arg, NULL);
+
+	if (fdt_addr)
+		fdt = map_sysmem(fdt_addr, 0);
+	else
+		fdt = EFI_FDT_USE_INTERNAL;
 
 	if (IS_ENABLED(CONFIG_CMD_BOOTEFI_BOOTMGR) &&
 	    !strcmp(argv[1], "bootmgr")) {
@@ -212,7 +227,7 @@ static int do_bootefi(struct cmd_tbl *cmdtp, int flag, int argc,
 		}
 	}
 
-	ret = efi_binary_run(image_buf, size, fdt, NULL, 0);
+	ret = efi_binary_run(image_buf, size, fdt, ramdisk, rd_len);
 
 	if (ret != EFI_SUCCESS)
 		return CMD_RET_FAILURE;
@@ -221,7 +236,7 @@ static int do_bootefi(struct cmd_tbl *cmdtp, int flag, int argc,
 }
 
 U_BOOT_LONGHELP(bootefi,
-	"<image address>[:<image size>] [<fdt address>]\n"
+	"<image address>[:<image size>] [<ramdisk_address>:<size>] [<fdt address>]\n"
 	"  - boot EFI payload\n"
 #ifdef CONFIG_CMD_BOOTEFI_HELLO
 	"bootefi hello\n"
