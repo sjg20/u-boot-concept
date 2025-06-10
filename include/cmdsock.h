7883387e9053694ab21aea48ce465596b7376af5
Simon Glass <sjg@chromium.org>
2025-06-10 06:51:08 -0600

    cmdsock: Support character output
    Provide a way for putc() and puts() to send the output over the cmdsock
    instead of to the terminal.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/cmdsock.h
+++ include/cmdsock.h
@@ -94,6 +94,30 @@ void cmdsock_init(struct membuf *in, struct membuf *out);
  */
 int cmdsock_process(void);
 
+/**
+ * cmdsock_putc() - Handle writing a character
+ *
+ * If a client is connected, this sends a message with the character.
+ *
+ * This must only be called if cmdsock_active()
+ *
+ * @ch: Character to send
+ * Return: 0 on success (always)
+ */
+int cmdsock_putc(int ch);
+
+/**
+ * cmdsock_puts() - Handle writing a string
+ *
+ * If a client is connected, this sends a message with the string.
+ *
+ * This must only be called if cmdsock_active()
+ *
+ * @str: String to send
+ * Return: 0 on success (always)
+ */
+int cmdsock_puts(const char *s);
+
 #ifdef CONFIG_CMDSOCK
 
 /**
