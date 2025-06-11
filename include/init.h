5c6c3da948f3d404644e7cada07300d360040b31
Simon Glass <sjg@chromium.org>
2025-06-10 19:59:14 -0600

    cmdsock: Plumb in the command socket to board init
    When a command-socket is used, don't enter the main loop. Instead enter
    the cmdsock look, which looks for new cient connections and requests.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/init.h
+++ include/init.h
@@ -277,8 +277,12 @@ struct global_data;
 void arch_setup_gd(struct global_data *gd_ptr);
 
 /* common/board_r.c */
+#ifndef CONFIG_CMDSOCK
 void board_init_r(struct global_data *id, ulong dest_addr)
 	__attribute__ ((noreturn));
+#else
+void board_init_r(struct global_data *id, ulong dest_addr);
+#endif
 
 int cpu_init_r(void);
 int mac_read_from_eeprom(void);
