81a0b7a12e57cb8bf404e4e03b9bd48601a7dc18
Simon Glass <sjg@chromium.org>
2025-06-17 12:38:32 -0600

    printenv

--- test/py/console_test.py
+++ test/py/console_test.py
@@ -125,7 +125,45 @@ class TestConsole(unittest.TestCase):
         cons = console_sandbox.ConsoleSandbox(ubc.log, ubc)
         cons.ensure_spawned()
 
+        self.maxDiff = None
         val = cons.run_command('printenv')
+        self.assertEqual('''arch=sandbox
+baudrate=115200
+board=sandbox
+board_name=sandbox
+bootcmd=bootflow scan -lb
+bootdelay=2
+bootm_size=0x10000000
+cpu=sandbox
+env_fdt_path=/config/environment
+eth2addr=02:00:11:22:33:48
+eth3addr=02:00:11:22:33:45
+eth4addr=02:00:11:22:33:48
+eth5addr=02:00:11:22:33:46
+eth6addr=02:00:11:22:33:47
+eth7addr=02:00:11:22:33:48
+eth8addr=02:00:11:22:33:49
+ethaddr=02:00:11:22:33:44
+fdt_addr_r=0xc00000
+fdtcontroladdr=8b1b310
+from_fdt=yes
+ipaddr=192.0.2.1
+ipaddr2=192.0.2.3
+ipaddr3=192.0.2.4
+ipaddr5=192.0.2.6
+ipaddr6=192.0.2.7
+ipaddr7=192.0.2.8
+kernel_addr_r=0x1000000
+loadaddr=0x0
+pxefile_addr_r=0x2000
+ramdisk_addr_r=0x2000000
+scriptaddr=0x1000
+stderr=serial,vidconsole
+stdin=serial,cros-ec-keyb,usbkbd
+stdout=serial,vidconsole
+usb_ignorelist=0x1050:*,
+
+Environment size: 756/8188 bytes''', '\n'.join(val.splitlines()))
 
         ubc.log.close()
         cons.cleanup_spawn()
