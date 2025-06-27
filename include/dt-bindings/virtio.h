48fa764914cdc9343fdff929927254bf8ccb32c0
Simon Glass <sjg@chromium.org>
2025-06-27 07:40:42 -0600

    virtio: Move bindings for virtio IDs to dt-bindings
    These values are used in the devicetree and the C code, so move them to
    a new dt-bindings header.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ include/dt-bindings/virtio.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2018, Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
+ * Copyright (C) 2018, Bin Meng <bmeng.cn@gmail.com>
+ *
+ * Binding file for virtio IDs
+ *
+ * This file is largely based on Linux kernel virtio_*.h files
+ */
+
+#ifndef __DT_BINDINGS_VIRTIO
+#define __DT_BINDINGS_VIRTIO
+
+#define VIRTIO_ID_NET		1 /* virtio net */
+#define VIRTIO_ID_BLOCK		2 /* virtio block */
+#define VIRTIO_ID_RNG		4 /* virtio rng */
+#define VIRTIO_ID_MAX_NUM	27
+
+#endif
