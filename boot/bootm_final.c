88c2352096221f318ce9df398fb272e679f9331c
Simon Glass <sjg@chromium.org>
2025-07-08 07:57:08 +0200

    bootm: Show a bootstage report when booting
    If enabled, show a bootstage report.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- boot/bootm_final.c
+++ boot/bootm_final.c
@@ -6,12 +6,18 @@
  */
 
 #include <bootm.h>
+#include <bootstage.h>
 #include <dm.h>
 #include <dm/device-internal.h>
 
 void bootm_final(void)
 {
 	printf("\nStarting kernel ...\n\n");
+
+	bootstage_mark_name(BOOTSTAGE_ID_BOOTM_HANDOFF, "start_kernel");
+#if IS_ENABLED(CONFIG_BOOTSTAGE_REPORT)
+	bootstage_report();
+#endif
 
 	dm_remove_devices_active();
 }
