ca30385c61e4124d57a16f0bc88930acc33a680c
Simon Glass <sjg@chromium.org>
2025-08-02 10:36:31 -0600

    Correct implementation of Spawn() in ConsoleBase
    Now that this base-class function is called, it can produce an error on
    test failure, since it passes an empty list for the arguments.
    
    Rename the reset() function to prepare_for_spawn() and use that instead.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/py/console_board.py
+++ test/py/console_board.py
@@ -55,7 +55,7 @@ class ConsoleExecAttach(ConsoleBase):
         Returns:
             A spawn.Spawn object that is attached to U-Boot.
         """
-        super().get_spawn()
+        self.prepare_for_spawn()
         args = [self.config.board_type, self.config.board_identity]
         s = Spawn(['u-boot-test-console'] + args)
 
