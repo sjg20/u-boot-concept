1a61f8a50e7db46942a2cd347188149fe343c45b
Simon Glass <sjg@chromium.org>
2025-08-07 11:37:15 -0600

    boot: pxe: Init initrd_str in label_boot()
    When 'bootflow read' is used to read images, initrd_str is not inited
    before strdup() is called. Note that this is only used by developers.
    
    Set the variable to an empty string to start.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- boot/pxe_utils.c
+++ boot/pxe_utils.c
@@ -642,7 +642,7 @@ static int label_boot(struct pxe_context *ctx, struct pxe_label *label)
 	char *kernel_addr = NULL;
 	char *initrd_addr_str = NULL;
 	char initrd_filesize[10];
-	char initrd_str[28];
+	char initrd_str[28] = "";
 	char mac_str[29] = "";
 	char ip_str[68] = "";
 	char *fit_addr = NULL;
