10e31d38f86f0bf63405c7217a33215ec6685ee2
Simon Glass <sjg@chromium.org>
2025-08-11 15:59:41 -0600

    efi: Exit EFI boot-services before starting the next app
    When the app is booting a kernel without using EFI, it must first exit
    the boot services provided by EFI. Add a hook for this, using
    bootm_final()
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- board/efi/efi-arm_app/board.c
+++ board/efi/efi-arm_app/board.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2015 Google, Inc
  */
 
+#include <bootm.h>
+#include <efi.h>
+#include <event.h>
 #include <init.h>
 
 struct mm_region *mem_map;
@@ -16,3 +19,30 @@ int board_init(void)
 {
 	return 0;
 }
+
+int board_exit_boot_services(void *ctx, struct event *evt)
+{
+	struct efi_priv *priv = efi_get_priv();
+	struct efi_mem_desc *desc;
+	int desc_size;
+	uint version;
+	int size;
+	uint key;
+	int ret;
+
+	printf("Exiting EFI\n");
+	ret = efi_get_mmap(&desc, &size, &key, &desc_size, &version);
+	if (ret) {
+		printf("efi: Failed to get memory map\n");
+		return -EFAULT;
+	}
+
+	ret = efi_app_exit_boot_services(priv, key);
+	if (ret)
+		return ret;
+
+	/* no console output after here as there are no EFI drivers! */
+
+	return 0;
+}
+EVENT_SPY_FULL(EVT_BOOTM_FINAL, board_exit_boot_services);
