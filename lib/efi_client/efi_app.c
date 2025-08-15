968ee73c16bde0eaaac5764491655654fe2d9014
Simon Glass <sjg@chromium.org>
2025-08-15 13:48:32 -0600

    efi: app: Tidy up some stale comments in setup_memory()
    A few comments are out of date. Drop the one about global_data_ptr and
    reword the one about memory above 4GB.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- lib/efi_client/efi_app.c
+++ lib/efi_client/efi_app.c
@@ -92,11 +92,6 @@ static efi_status_t setup_memory(struct efi_priv *priv)
 	efi_status_t ret;
 	int pages;
 
-	/*
-	 * Use global_data_ptr instead of gd since it is an assignment. There
-	 * are very few assignments to global_data in U-Boot and this makes
-	 * it easier to find them.
-	 */
 	ptr = efi_malloc(priv, sizeof(*ptr), &ret);
 	if (!ptr)
 		return ret;
@@ -111,8 +106,8 @@ static efi_status_t setup_memory(struct efi_priv *priv)
 	pages = CONFIG_EFI_RAM_SIZE >> 12;
 
 	/*
-	 * Don't allocate any memory above 4GB. U-Boot is a 32-bit application
-	 * so we want it to load below 4GB.
+	 * Try not to allocate any memory above 4GB, just for ease of looking at
+	 * addresses.
 	 */
 	addr = 1ULL << 32;
 	ret = boot->allocate_pages(EFI_ALLOCATE_MAX_ADDRESS,
