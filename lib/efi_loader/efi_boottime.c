f06ec4fbcd81938ceb49287ecef0848c2080db09
Simon Glass <sjg@chromium.org>
2025-08-16 10:39:12 -0600

    efi: Move driver-binding guid to a common file
    Move efi_guid_driver_binding_protocol to lib/efi so that it can be used
    from the app.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- lib/efi_loader/efi_boottime.c
+++ lib/efi_loader/efi_boottime.c
@@ -68,9 +68,6 @@ efi_status_t efi_uninstall_protocol
 /* 1 if inside U-Boot code, 0 if inside EFI payload code */
 static int entry_count = 1;
 static int nesting_level;
-/* GUID of the EFI_DRIVER_BINDING_PROTOCOL */
-const efi_guid_t efi_guid_driver_binding_protocol =
-			EFI_DRIVER_BINDING_PROTOCOL_GUID;
 
 /* event group ExitBootServices() invoked */
 const efi_guid_t efi_guid_event_group_exit_boot_services =
