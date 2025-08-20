e4ae410b748c07f4ccbe7e4b27a66301a3dfd487
Simon Glass <sjg@chromium.org>
2025-08-20 09:36:22 -0600

    efi: app: Don't bother showing the memory details in banner
    This information is not useful to users, only for debugging. So use
    log_debug() for it.
    
    Series-to: concept
    Series-cc: heinrich
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Suggested-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
    Series-links: 1:14

--- lib/efi_client/efi_app.c
+++ lib/efi_client/efi_app.c
@@ -116,19 +116,19 @@ static efi_status_t setup_memory(struct efi_priv *priv)
 	ret = boot->allocate_pages(EFI_ALLOCATE_MAX_ADDRESS,
 				   priv->image_data_type, pages, &addr);
 	if (ret) {
-		log_info("(any address) ");
+		log_debug("(any address) ");
 		ret = boot->allocate_pages(EFI_ALLOCATE_ANY_PAGES,
 					   priv->image_data_type, pages, &addr);
 	}
 	if (ret) {
-		log_info("(using pool %lx) ", ret);
+		log_debug("(using pool %lx) ", ret);
 		priv->ram_base = (ulong)efi_malloc(priv, CONFIG_EFI_RAM_SIZE,
 						   &ret);
 		if (!priv->ram_base)
 			return ret;
 		priv->use_pool_for_malloc = true;
 	} else {
-		log_info("(using allocated RAM address %lx) ", (ulong)addr);
+		log_debug("(using allocated RAM address %lx) ", (ulong)addr);
 		priv->ram_base = addr;
 	}
 	gd->ram_base = addr;
