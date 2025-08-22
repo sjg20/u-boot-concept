15d9fa8518116824b56928e53a4c4f821ac9c804
Simon Glass <sjg@chromium.org>
2025-08-21 18:49:56 -0600

    console: Support paging with single characters
    A single character may result in a stall waiting for user input, which
    means that it may request that a string be output. So when the pager is
    active we never actually use the devices' putc() methods. Add a special
    case so they don't go to wrack and ruin.
    
    As before, the pager is only supported with CONFIG_CONSOLE_MUX enabled.
    
    Series-changes: 2
    - Drop unnecessary '!= NULL'
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- common/console.c
+++ common/console.c
@@ -322,14 +322,22 @@ static int console_tstc(int file)
 	return 0;
 }
 
+static void console_puts_pager(int file, const char *s);
+
 static void console_putc_pager(int file, const char c)
 {
-	int i;
-	struct stdio_dev *dev;
+	if (IS_ENABLED(CONFIG_CONSOLE_PAGER) && gd_pager()) {
+		char str[2] = {c, '\0'};
 
-	for_each_console_dev(i, file, dev) {
-		if (dev->putc != NULL)
-			dev->putc(dev, c);
+		console_puts_pager(file, str);
+	} else {
+		int i;
+		struct stdio_dev *dev;
+
+		for_each_console_dev(i, file, dev) {
+			if (dev->putc)
+				dev->putc(dev, c);
+		}
 	}
 }
 
