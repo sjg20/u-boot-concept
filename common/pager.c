56483c8367b5eb690f2e8cda8bf77f7cdd54cf10
Simon Glass <sjg@chromium.org>
2025-08-21 18:06:07 -0600

    console: Add the basic pager implementation
    Add an implementation of the pager. It has quite a simple API:
    
      - call pager_post() to send some output, which returns what should
        actually be sent to the console devices
      - then call pager_next() repeatedly, outputting what it returns,
        until it returns NULL
    
    There is one special case: pager_next() returns PAGER_WAITING if it is
    waiting for the user to press a key. In that case, the caller should
    read a key and then pass it to the next pager_next() call.
    
    Internally, there is a simple state machine to cope with hitting the
    limit (and outputing a prompt), waiting for the user and the clearing
    the prompt, before continuing in the normal PAGEST_OK state.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ common/pager.c
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Deals with splitting up text output into separate screenfuls
+ *
+ * Copyright 2025 Simon Glass <sjg@chromium.org>
+ */
+
+#define LOG_CATEGORY LOGC_CONSOLE
+
+#include <errno.h>
+#include <malloc.h>
+#include <pager.h>
+#include <asm/global_data.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+const char *pager_post(struct pager *pag, const char *s)
+{
+	struct membuf old;
+	int ret, len;
+
+	if (!pag)
+		return s;
+
+	len = strlen(s);
+	if (!len)
+		return NULL;
+
+	old = pag->mb;
+	ret = membuf_put(&pag->mb, s, len);
+	if (ret == len) {
+		/* all is well */
+	} else {
+		/*
+		 * We couldn't store any of the text, so we'll store none of
+		 * it. The pager is now in an non-functional state until it
+		 * can eject the overflow text.
+		 *
+		 * The buffer is presumably empty, since callers are not allowed
+		 * to call pager_post() unless all the output from the previous
+		 * call was provided via pager_next().
+		 */
+		pag->overflow = s;
+		pag->mb = old;
+	}
+
+	return pager_next(pag, 0);
+}
+
+const char *pager_next(struct pager *pag, int key)
+{
+	char *str, *p, *end;
+	int ret;
+
+	/* replace the real character we overwrite with nul, if needed */
+	if (pag->nulch) {
+		*pag->nulch = pag->oldch;
+		pag->nulch = NULL;
+	}
+
+	/* if we're at the limit, wait */
+	switch (pag->state) {
+	case PAGERST_OK:
+		break;
+	case PAGERST_AT_LIMIT:
+		pag->state = PAGERST_WAIT_USER;
+		return PAGER_PROMPT;
+	case PAGERST_WAIT_USER:
+		if (key != ' ')
+			return PAGER_WAITING;
+		pag->state = PAGERST_CLEAR_PROMPT;
+		return PAGER_BLANK;
+	case PAGERST_CLEAR_PROMPT:
+		pag->state = PAGERST_OK;
+		break;
+	}
+
+	ret = membuf_getraw(&pag->mb, pag->buf.size - 1, false, &str);
+	if (!ret) {
+		if (pag->overflow) {
+			const char *oflow = pag->overflow;
+
+			pag->overflow = NULL;
+			return oflow;
+		}
+		return NULL;
+	}
+
+	/* return lines until we reach the limit */
+	for (p = str, end = str + ret; p < end; p++) {
+		if (*p == '\n' && ++pag->line_count == pag->page_len - 1) {
+			/* remember to display the pager message next time */
+			pag->state = PAGERST_AT_LIMIT;
+			pag->line_count = 0;
+
+			/* skip the newline, since our prompt has one */
+			p++;
+			break;
+		}
+	}
+
+	/* remove the used bytes from the membuf */
+	ret = membuf_getraw(&pag->mb, p - str, true, &str);
+
+	/* don't output the newline, since our prompt has one */
+	if (pag->state == PAGERST_AT_LIMIT)
+		p--;
+
+	/* terminate the string */
+	pag->nulch = p;
+	pag->oldch = *pag->nulch;
+	*pag->nulch = '\0';
+
+	return str;
+}
+
+void pager_uninit(struct pager *pag)
+{
+	abuf_uninit(&pag->buf);
+	free(pag);
+}
+
+int pager_init(struct pager **pagp, int page_len, int buf_size)
+{
+	struct pager *pag;
+
+	pag = malloc(sizeof(struct pager));
+	if (!pag)
+		return log_msg_ret("pag", -ENOMEM);
+	memset(pag, '\0', sizeof(struct pager));
+	pag->page_len = page_len;
+	if (!abuf_init_size(&pag->buf, buf_size))
+		return log_msg_ret("pah", -ENOMEM);
+
+	/*
+	 * nul-terminate the buffer, which will come in handy if we need to
+	 * return up to the last byte
+	 */
+	((char *)pag->buf.data)[buf_size - 1] = '\0';
+	membuf_init(&pag->mb, pag->buf.data, buf_size);
+	*pagp = pag;
+
+	return 0;
+}
