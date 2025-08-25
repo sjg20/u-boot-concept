f75ea56c11b040809769d28b3bbcd8dee64ca7c2
Simon Glass <sjg@chromium.org>
2025-08-25 15:12:00 -0600

    test/py: Disable the pager when running sandbox tests
    The pager gets in the way of most tests, since it kicks in (by default)
    when a command results in more than 25 lines of output. Use the -P flag
    to bypass it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/py/console_sandbox.py
+++ test/py/console_sandbox.py
@@ -52,6 +52,10 @@ class ConsoleSandbox(ConsoleBase):
         if self.use_dtb:
             cmd += ['-d', self.config.dtb]
         cmd += self.sandbox_flags
+
+        # Always disable the pager
+        cmd.append('-P')
+
         return Spawn(cmd, cwd=self.config.source_dir, decode_signal=True)
 
     def restart_uboot_with_flags(self, flags, use_dtb=True):
