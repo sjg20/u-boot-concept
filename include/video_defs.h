d16fbb3f524492e7fa87612939b984f303ce5d3f
Simon Glass <sjg@chromium.org>
2025-10-01 14:07:41 -0600

    video: Support a linker list of images
    It is inconvenient to have to access graphical images as independent
    symbols. Create a new rule which handles any file mentioned in
    drivers/video/images/Makefile
    
    For each graphical image, embed in the image and create a linker-list
    entry for it.
    
    Series-changes: 2
    - Use Canonical copyright
    
    Series-changes: 3
    - Rename it to video_defs.h since we will use it elsewhere
    
    Co-developed-by: Claude <noreply@anthropic.com>
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ include/video_defs.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Video definitions
+ *
+ * Copyright 2025 Canonical Ltd
+ * Written by Simon Glass <simon.glass@canonical.com>
+ */
+
+#ifndef __VIDEO_DEFS_H
+#define __VIDEO_DEFS_H
+
+/* Maximum length of an embedded image name */
+#define VIDEO_IMAGE_NAMELEN	16
+
+#endif /* __VIDEO_DEFS_H */
