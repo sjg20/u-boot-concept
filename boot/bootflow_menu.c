sjg20/u-boot-concept

commit e94de63e6c89239e14ed3bbdb1d28a2b189de332

expo: Add a way to select settings
In some cases it is useful to provide a settings scene, or perhaps just
a button to change the layout. Add support for this.

boot/bootflow_menu.c
@@ -314,6 +314,8 @@ int bootflow_menu_poll(struct expo *exp, int *seqp)
 	case EXPOACT_QUIT:
 		return -EPIPE;
 	case EXPOACT_CLICK:
+		if (act.select.id == OBJ_SETTINGS)
+			return -ECOMM;  /* layout change request */
 	default:
 		return -EAGAIN;
 	}

boot/bootflow_internal.h
@@ -13,6 +13,7 @@
  * enum boomenu_id_t - expo IDs for elements of the bootflow menu
  *
  * @OBJ_OTHER_LOGO: Second logo (separate from the U-Boot logo)
+ * @OBJ_SETTINGS: Select settings / change layout
  *
  * The ranges below are as follows:
  *
@@ -47,6 +48,7 @@ enum boomenu_id_t {
 	OBJ_POINTER,
 	OBJ_AUTOBOOT,
 	OBJ_OTHER_LOGO,
+	OBJ_SETTINGS,
 
 	/* strings for menu items */
 	STR_LABEL = 100,

boot/cedit.c
@@ -221,6 +221,7 @@ int cedit_do_action(struct expo *exp, struct scene *scn,
 		exp->done = true;
 		break;
 	case EXPOACT_CLICK:
+	case EXPOACT_SETTINGS:
 		/* not supported by cedit */
 		break;
 	}

boot/scene_menu.c
@@ -485,6 +485,9 @@ int scene_menu_send_key(struct scene *scn, struct scene_obj_menu *menu, int key,
 			log_debug("menu quit\n");
 		}
 		break;
+	case ' ':
+		event->type = EXPOACT_SETTINGS;
+		break;
 	case '0'...'9':
 		key_item = scene_menu_find_key(scn, menu, key);
 		if (key_item) {

include/bootflow.h
@@ -743,7 +743,8 @@ int bootflow_menu_start(struct bootstd_priv *std, bool text_mode,
  * Return: 0 if a bootflow was chosen, -EAGAIN if nothing is chosen yet, -EPIPE
  *	if the user quit, -EREMCHG if the expo needs refreshing, -ERESTART if
  *	the user tried to move to a new selection but was unable (e.g. already
- *	at the top and tried to move up)
+ *	at the top and tried to move up), -ECOMM if the user requests settings
+ *	to be opened
  */
 int bootflow_menu_poll(struct expo *exp, int *seqp);
 

include/expo.h
@@ -57,6 +57,7 @@ enum expo_id_t {
  * the menu closed, @id indicates menu opened)
  * @EXPOACT_QUIT: request to exit the menu
  * @EXPOACT_CLICK: click on an object
+ * @EXPOACT_SETTINGS: select menu settings
  */
 enum expoact_type {
 	EXPOACT_NONE,
@@ -70,6 +71,7 @@ enum expoact_type {
 	EXPOACT_REPOINT_OPEN,
 	EXPOACT_QUIT,
 	EXPOACT_CLICK,
+	EXPOACT_SETTINGS,
 };
 
 /**

commit f56c1b0238c8970430b222daab6d53634d0a3925

expo: Correct handling of mouse clicks
Due to a missing return in bootflow_menu_poll() a click on any object is
handled as if it were a click on the settings object. Fix this by
returning the correct error code for unrecognised clicks.

Signed-off-by: Simon Glass <sjg@chromium.org>
Fixes: e94de63e6c8 ("expo: Add a way to select settings")

boot/bootflow_menu.c
@@ -418,6 +418,7 @@ int bootflow_menu_poll(struct expo *exp, int *seqp)
 	case EXPOACT_CLICK:
 		if (act.select.id == OBJ_SETTINGS)
 			return -ECOMM;  /* layout change request */
+		return -EAGAIN;
 	case EXPOACT_SETTINGS:
 		return -ECOMM;  /* layout change request */
 	default: